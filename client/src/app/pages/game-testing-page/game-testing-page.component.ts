9753884b34e0eb2df488e25d62c5e1b77ed5f29b
umi2002 <jonathanhoang2002@gmail.com>
2024-01-20 12:26:32 -0500

    Added functions to retrieve game data and question data

--- client/src/app/pages/game-testing-page/game-testing-page.component.ts
+++ client/src/app/pages/game-testing-page/game-testing-page.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GameData } from '@common/game-data';
+import { QuestionData } from '@common/question-data';
 
 @Component({
     selector: 'app-game-testing-page',
     templateUrl: './game-testing-page.component.html',
     styleUrls: ['./game-testing-page.component.scss'],
 })
-export class GameTestingPageComponent {}
+export class GameTestingPageComponent implements OnInit {
+    @Input() gameId: number;
+
+    gameData: GameData;
+    questionData: QuestionData;
+    currentQuestionIndex: number = 0;
+
+    ngOnInit(): void {
+        this.getGameData();
+        this.getQuestionData(this.currentQuestionIndex);
+    }
+
+    getGameData(): void {
+        const testGame: GameData = {
+            id: 0,
+            name: 'Math',
+            questionIds: [],
+        };
+
+        this.gameData = testGame;
+    }
+
+    getQuestionData(id: number): void {
+        const testQuestion: QuestionData = {
+            id: 0,
+            value: 1,
+            question: '1+1=?',
+            answers: ['1', '2', '3', '4'],
+            correctAnswers: ['2'],
+            isMCQ: true,
+        };
+
+        this.questionData = testQuestion;
+    }
+}
