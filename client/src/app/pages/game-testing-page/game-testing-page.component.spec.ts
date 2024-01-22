dbc3ea25f61fdbdf7f41766f4bfde648b89981f9
umi2002 <jonathanhoang2002@gmail.com>
2024-01-22 12:22:16 -0500

    Fixed linting

--- client/src/app/pages/game-testing-page/game-testing-page.component.spec.ts
+++ client/src/app/pages/game-testing-page/game-testing-page.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameTestingPageComponent } from './game-testing-page.component';
 
 describe('GameTestingPageComponent', () => {
-  let component: GameTestingPageComponent;
-  let fixture: ComponentFixture<GameTestingPageComponent>;
+    let component: GameTestingPageComponent;
+    let fixture: ComponentFixture<GameTestingPageComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [GameTestingPageComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [GameTestingPageComponent],
+        });
+        fixture = TestBed.createComponent(GameTestingPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(GameTestingPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
