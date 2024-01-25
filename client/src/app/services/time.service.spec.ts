b46a20af9c354d6c29065fa250ae36f4ac623e98
umi2002 <jonathanhoang2002@gmail.com>
2024-01-24 22:55:39 -0500

    Tests written for timer class and time service

--- client/src/app/services/time.service.spec.ts
+++ client/src/app/services/time.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TimeService } from './time.service';
+import { Timer } from '@app/classes/timer';
+import { TimeService } from '@app/services/time.service';
 
 describe('TimeService', () => {
     let service: TimeService;
+    const TIMER_ID = 0;
+    const TIMEOUT = 5;
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
@@ -18,9 +21,107 @@ describe('TimeService', () => {
         expect(service.createTimer()).toEqual(jasmine.any(Number));
     });
 
-    it('multiple calls to createTimer should return different Timers', () => {
-        const timer1 = service.createTimer();
-        const timer2 = service.createTimer();
-        expect(timer1).not.toBe(timer2);
+    it('createTimer should return a unique number each time', () => {
+        const firstId = service.createTimer();
+        const secondId = service.createTimer();
+
+        expect(firstId).not.toEqual(secondId);
+    });
+
+    it('createTimer should add a timer to the map with the correct id', () => {
+        const timerId = service.createTimer();
+        const timer = service['timers'].get(timerId);
+
+        expect(timer).toBeTruthy();
+        expect(timer).toBeInstanceOf(Timer);
+    });
+
+    it('createTimer should add a timer with callback to the map with the correct id', () => {
+        const callbackSpy = jasmine.createSpy('callback');
+        const timerId = service.createTimer(callbackSpy);
+        const timer = service['timers'].get(timerId);
+
+        expect(timer).toBeTruthy();
+        expect(timer).toBeInstanceOf(Timer);
+    });
+
+    it('startTimer should start the timer with the correct id', () => {
+        const mockTimer = jasmine.createSpyObj('Timer', ['start']);
+        spyOn(service, 'createTimer').and.callFake(() => {
+            service['timers'].set(TIMER_ID, mockTimer);
+            return TIMER_ID;
+        });
+
+        const timerId = service.createTimer();
+        service.startTimer(timerId, 0);
+
+        expect(mockTimer.start).toHaveBeenCalled();
+    });
+
+    it('startTimer should not start the timer with the incorrect id', () => {
+        const mockTimer = jasmine.createSpyObj('Timer', ['start']);
+        spyOn(service, 'createTimer').and.callFake(() => {
+            service['timers'].set(TIMER_ID, mockTimer);
+            return TIMER_ID;
+        });
+
+        const timerId = service.createTimer();
+        service.startTimer(timerId + 1, 0);
+
+        expect(mockTimer.start).not.toHaveBeenCalled();
+    });
+
+    it('stopTimer should stop the timer with the correct id', () => {
+        const mockTimer = jasmine.createSpyObj('Timer', ['stop']);
+        spyOn(service, 'createTimer').and.callFake(() => {
+            service['timers'].set(TIMER_ID, mockTimer);
+            return TIMER_ID;
+        });
+
+        const timerId = service.createTimer();
+        service.stopTimer(timerId);
+
+        expect(mockTimer.stop).toHaveBeenCalled();
+    });
+
+    it('stopTimer should not stop the timer with the incorrect id', () => {
+        const mockTimer = jasmine.createSpyObj('Timer', ['stop']);
+        spyOn(service, 'createTimer').and.callFake(() => {
+            service['timers'].set(TIMER_ID, mockTimer);
+            return TIMER_ID;
+        });
+
+        const timerId = service.createTimer();
+        service.stopTimer(timerId + 1);
+
+        expect(mockTimer.stop).not.toHaveBeenCalled();
+    });
+
+    it('getTime should return the correct time for the timer with the correct id', () => {
+        const mockTimer = jasmine.createSpyObj('Timer', ['time']);
+        mockTimer['time'] = TIMEOUT;
+        spyOn(service, 'createTimer').and.callFake(() => {
+            service['timers'].set(TIMER_ID, mockTimer);
+            return TIMER_ID;
+        });
+
+        const timerId = service.createTimer();
+        const time = service.getTime(timerId);
+
+        expect(time).toEqual(TIMEOUT);
+    });
+
+    it('getTime should return 0 for the timer with the incorrect id', () => {
+        const mockTimer = jasmine.createSpyObj('Timer', ['time']);
+        mockTimer['time'] = TIMEOUT;
+        spyOn(service, 'createTimer').and.callFake(() => {
+            service['timers'].set(TIMER_ID, mockTimer);
+            return TIMER_ID;
+        });
+
+        const timerId = service.createTimer();
+        const time = service.getTime(timerId + 1);
+
+        expect(time).toEqual(0);
     });
 });
