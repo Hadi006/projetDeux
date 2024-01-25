81ba7245342dda182320f2100880c2fe91a34dbd
RajabFawaz <90001583+RajabFawaz@users.noreply.github.com>
2024-01-25 10:07:37 -0500

    now create-game is done

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { GameChoicePageComponent } from '@app/pages/game-choice-page/game-choice-page.component';
-=======
-import { ChoixJeuComponent } from '@app/choix-jeu/choix-jeu.component';
->>>>>>> a94627a53aecd21dcb791fd7aaa933cf07329a8d
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { QuestionsPageComponent } from '@app/pages/questions-page/questions-page.component';
@@ -12,15 +8,9 @@ import { QuestionsPageComponent } from '@app/pages/questions-page/questions-page
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
-<<<<<<< HEAD
     { path: 'game', component: GameChoicePageComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: 'questions/:jeu', component: QuestionsPageComponent },
-=======
-    { path: 'game', component: ChoixJeuComponent },
-    { path: 'material', component: MaterialPageComponent },
-    { path: 'questions/:jeu', component: QuestionsComponent },
->>>>>>> a94627a53aecd21dcb791fd7aaa933cf07329a8d
     { path: '**', redirectTo: '/home' },
 ];
 
