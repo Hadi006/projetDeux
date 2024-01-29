87423a2f7b7c7699ae41fbb6004c86a447a28042
umi2002 <jonathanhoang2002@gmail.com>
2024-01-29 10:11:17 -0500

    Migrating game-testing page to gameplay-player-page

--- client/src/app/pages/gameplay-player-page/gameplay-player-page.component.spec.ts
+++ client/src/app/pages/gameplay-player-page/gameplay-player-page.component.spec.ts
@@ -1,15 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { GameplayPlayerPageComponent } from '@app/pages/gameplay-player-page/gameplay-player-page.component';
+import { GameHandlerService, GameState } from '@app/services/game-handler.service';
+import { BehaviorSubject } from 'rxjs';
 
 describe('GameplayPlayerPageComponent', () => {
     let component: GameplayPlayerPageComponent;
     let fixture: ComponentFixture<GameplayPlayerPageComponent>;
+    let gameHandlerServiceSpy: jasmine.SpyObj<GameHandlerService>;
+    let gameStateSubjectSpy: BehaviorSubject<GameState>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
     beforeEach(() => {
+        gameStateSubjectSpy = new BehaviorSubject<GameState>(GameState.ShowQuestion);
+        gameHandlerServiceSpy = jasmine.createSpyObj('GameHandlerService', ['stateSubject', 'startGame'], {
+            stateSubject: gameStateSubjectSpy,
+        });
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    });
+
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [GameplayPlayerPageComponent],
-        });
+            providers: [
+                { provide: GameHandlerService, useValue: gameHandlerServiceSpy },
+                { provide: Router, useValue: routerSpy },
+            ],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
         fixture = TestBed.createComponent(GameplayPlayerPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -17,5 +38,48 @@ describe('GameplayPlayerPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should set answerConfirmed and showingAnswer to false when the game state is ShowQuestion', () => {
+        component.gameHandlerService.stateSubject.next(GameState.ShowQuestion);
+
+        expect(component.answerConfirmed).toBeFalse();
+        expect(component.showingAnswer).toBeFalse();
+    });
+
+    it('should set answerConfirmed and showingAnswer to true when the game state is ShowAnswer', () => {
+        component.gameHandlerService.stateSubject.next(GameState.ShowAnswer);
+
+        expect(component.answerConfirmed).toBeTrue();
+        expect(component.showingAnswer).toBeTrue();
+    });
+
+    it('should navigate to the home page when the game state is GameEnded', () => {
+        component.gameHandlerService.stateSubject.next(GameState.GameEnded);
+        expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
+    });
+
+    it('should set answerConfirmed to true when the Enter key is pressed', () => {
+        const event = new KeyboardEvent('keyup', { key: 'Enter' });
+        component.handleKeyUp(event);
+
+        expect(component.answerConfirmed).toBeTrue();
+    });
+
+    it('should not set answerConfirmed to true when a key other than Enter is pressed', () => {
+        const event = new KeyboardEvent('keyup', { key: 'Space' });
+        component.handleKeyUp(event);
+
+        expect(component.answerConfirmed).toBeFalse();
+    });
+
+    it('should call gameHandlerService.startGame() when ngOnInit() is called', () => {
+        component.ngOnInit();
+        expect(gameHandlerServiceSpy.startGame).toHaveBeenCalled();
+    });
+
+    it('should unsubscribe from gameStateSubscription when ngOnDestroy() is called', () => {
+        component.ngOnDestroy();
+        expect(component['gameStateSubscription'].closed).toBeTrue();
     });
 });
--- client/src/app/pages/gameplay-player-page/gameplay-player-page.component.ts
+++ client/src/app/pages/gameplay-player-page/gameplay-player-page.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
-import { GameHandlerService } from '@app/services/game-handler.service';
+import { Component, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GameHandlerService, GameState } from '@app/services/game-handler.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-gameplay-player-page',
     templateUrl: './gameplay-player-page.component.html',
     styleUrls: ['./gameplay-player-page.component.scss'],
 })
-export class GameplayPlayerPageComponent {
-    constructor(public gameHandler: GameHandlerService) {}
+export class GameplayPlayerPageComponent implements OnInit, OnDestroy {
+    @Input() gameId: number;
+
+    answerConfirmed: boolean = false;
+    showingAnswer: boolean = false;
+    gameState = GameState;
+
+    private gameStateSubscription: Subscription;
+
+    constructor(
+        public gameHandlerService: GameHandlerService,
+        public router: Router,
+    ) {
+        this.gameStateSubscription = this.gameHandlerService.stateSubject.subscribe((state: GameState) => {
+            switch (state) {
+                case GameState.ShowQuestion:
+                    this.answerConfirmed = false;
+                    this.showingAnswer = false;
+                    break;
+                case GameState.ShowAnswer:
+                    this.answerConfirmed = true;
+                    this.showingAnswer = true;
+                    break;
+                case GameState.GameEnded:
+                    this.router.navigate(['/']);
+                    break;
+            }
+        });
+    }
+
+    @HostListener('window:keyup', ['$event'])
+    handleKeyUp(event: KeyboardEvent): void {
+        if (!(event.key === 'Enter')) {
+            return;
+        }
+
+        this.answerConfirmed = true;
+    }
+
+    ngOnInit(): void {
+        this.gameHandlerService.startGame();
+    }
+
+    ngOnDestroy(): void {
+        this.gameStateSubscription.unsubscribe();
+    }
 }
