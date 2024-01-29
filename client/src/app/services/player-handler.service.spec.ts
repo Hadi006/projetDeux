3723db0bfa1d5c429b68899b150356ee12fa3050
umi2002 <jonathanhoang2002@gmail.com>
2024-01-29 15:56:52 -0500

    Added tests for player handler service

--- client/src/app/services/player-handler.service.spec.ts
+++ client/src/app/services/player-handler.service.spec.ts
@@ -1,16 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
 
 import { PlayerHandlerService } from './player-handler.service';
 
+const ANSWER_CONFIRMED_NOTIFIER_SUBJECTS = [new Subject<void>(), new Subject<void>(), new Subject<void>()];
+
 describe('PlayerHandlerService', () => {
-  let service: PlayerHandlerService;
+    let service: PlayerHandlerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PlayerHandlerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(PlayerHandlerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('answerConfirmedNotifiers should return the correct value', () => {
+        service['answerConfirmedNotifierSubjects'] = ANSWER_CONFIRMED_NOTIFIER_SUBJECTS;
+        expect(service.answerConfirmedNotifiers).toEqual(ANSWER_CONFIRMED_NOTIFIER_SUBJECTS);
+    });
 });
