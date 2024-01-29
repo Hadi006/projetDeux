020096814a30bc5f44c3effa4f5919e7aea631e3
Your Name <mikaelcarrenard@yahoo.ca>
2024-01-29 12:01:17 -0500

    chat-box component

--- client/src/app/components/chatbox/chatbox.component.ts
+++ client/src/app/components/chatbox/chatbox.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-chatbox',
-  templateUrl: './chatbox.component.html',
-  styleUrls: ['./chatbox.component.scss']
+    selector: 'app-chatbox',
+    templateUrl: './chatbox.component.html',
+    styleUrls: ['./chatbox.component.scss'],
 })
 export class ChatboxComponent {
+    showChat = true;
+    messages: { text: string; timestamp: Date }[] = [];
+    newMessage = '';
 
+    sendMessage() {
+        if (this.newMessage.trim() !== '') {
+            const newMessage = {
+                text: this.newMessage,
+                timestamp: new Date(),
+            };
+            this.messages.push(newMessage);
+            this.newMessage = '';
+        }
+    }
 }
