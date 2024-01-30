b79f26989023497a13b67b28f4ea08e18e5a3ebb
umi2002 <jonathanhoang2002@gmail.com>
2024-01-30 17:09:56 -0500

    Added tests for createAnswerTimer and startQuestionTimer. Added constants

--- client/src/app/services/game-timers.service.spec.ts
+++ client/src/app/services/game-timers.service.spec.ts
@@ -4,6 +4,11 @@ import { GameTimersService } from './game-timers.service';
 import { TimeService } from './time.service';
 
 describe('GameTimersService', () => {
+    const TIME_OUT = 10;
+    const TIMER_IDS = [0, 1];
+    const QUESTION_ID_INDEX = 0;
+    const ANSWER_ID_INDEX = 1;
+
     let service: GameTimersService;
     let timeServiceSpy: jasmine.SpyObj<TimeService>;
 
@@ -31,5 +36,23 @@ describe('GameTimersService', () => {
 
         expect(timeServiceSpy.createTimer).toHaveBeenCalledWith(callback);
         expect(service['questionTimerId']).toEqual(0);
+    });
+
+    it('createAnswerTimer should call createTimer with the correct callback and assign an id to answerTimerId', () => {
+        const callback = () => {
+            return;
+        };
+        timeServiceSpy.createTimer.and.returnValue(1);
+        service.createAnswerTimer(callback);
+
+        expect(timeServiceSpy.createTimer).toHaveBeenCalledWith(callback);
+        expect(service['answerTimerId']).toEqual(1);
+    });
+
+    it('startQuestionTimer should call startTimer with the correct id and time', () => {
+        service['questionTimerId'] = TIMER_IDS[QUESTION_ID_INDEX];
+        service.startQuestionTimer(TIME_OUT);
+
+        expect(timeServiceSpy.startTimer).toHaveBeenCalledWith(TIMER_IDS[QUESTION_ID_INDEX], TIME_OUT);
     });
 });
