4cf82855d6f651b91ce95ffea8c9f1457ddb6a53
umi2002 <jonathanhoang2002@gmail.com>
2024-01-30 17:06:47 -0500

    Added tests for createQuestionTimer

--- client/src/app/services/game-timers.service.spec.ts
+++ client/src/app/services/game-timers.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GameTimersService } from './game-timers.service';
+import { TimeService } from './time.service';
 
 describe('GameTimersService', () => {
-  let service: GameTimersService;
+    let service: GameTimersService;
+    let timeServiceSpy: jasmine.SpyObj<TimeService>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GameTimersService);
-  });
+    beforeEach(() => {
+        timeServiceSpy = jasmine.createSpyObj('TimeService', ['createTimer', 'startTimer', 'stopTimer', 'getTime', 'setTime']);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [{ provide: TimeService, useValue: timeServiceSpy }],
+        });
+        service = TestBed.inject(GameTimersService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('createQuestionTimer should call createTimer with the correct callback and assign an id to questionTimerId', () => {
+        const callback = () => {
+            return;
+        };
+        timeServiceSpy.createTimer.and.returnValue(0);
+        service.createQuestionTimer(callback);
+
+        expect(timeServiceSpy.createTimer).toHaveBeenCalledWith(callback);
+        expect(service['questionTimerId']).toEqual(0);
+    });
 });
