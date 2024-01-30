a14d5daf0ebb4719c8fede9443a3080413c59c96
umi2002 <jonathanhoang2002@gmail.com>
2024-01-30 13:10:10 -0500

    Added mock players data

--- client/src/app/services/player-handler.service.spec.ts
+++ client/src/app/services/player-handler.service.spec.ts
@@ -1,6 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { Player } from '@app/interfaces/player';
+import { Subject } from 'rxjs';
 
 import { PlayerHandlerService } from './player-handler.service';
+
+const MOCK_PLAYERS = new Map<number, Player>([0, 1, 2].map((id) => [id, { score: 0, answerNotifier: new Subject<boolean[]>() }]));
 
 describe('PlayerHandlerService', () => {
     let service: PlayerHandlerService;
