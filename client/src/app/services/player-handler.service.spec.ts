59ba32a1fe9c3f85f41af862829983534cf08278
umi2002 <jonathanhoang2002@gmail.com>
2024-01-30 13:06:49 -0500

    Reset tests for new implementation of player handler service

--- client/src/app/services/player-handler.service.spec.ts
+++ client/src/app/services/player-handler.service.spec.ts
@@ -1,9 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { Subject } from 'rxjs';
 
 import { PlayerHandlerService } from './player-handler.service';
-
-const ANSWER_CONFIRMED_NOTIFIER_SUBJECTS = [new Subject<void>(), new Subject<void>(), new Subject<void>()];
 
 describe('PlayerHandlerService', () => {
     let service: PlayerHandlerService;
@@ -15,10 +12,5 @@ describe('PlayerHandlerService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
-    });
-
-    it('gradeNotifiers should return the correct value', () => {
-        service['gradeNotifierSubjects'] = ANSWER_CONFIRMED_NOTIFIER_SUBJECTS;
-        expect(service.gradeNotifiers).toEqual(ANSWER_CONFIRMED_NOTIFIER_SUBJECTS);
     });
 });
