993562d9d7f5848bb3af08d1b87c282bb91700f4
umi2002 <jonathanhoang2002@gmail.com>
2024-01-31 01:21:31 -0500

    Added test for currentQuestion getter

--- client/src/app/services/question-handler.service.spec.ts
+++ client/src/app/services/question-handler.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuestionHandlerService } from './question-handler.service';
+import { QUESTION_DATA } from '@app/services/game-handler.service';
 
 describe('QuestionHandlerService', () => {
     let service: QuestionHandlerService;
@@ -12,5 +13,10 @@ describe('QuestionHandlerService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('currentQuestion should return the current question', () => {
+        service.setQuestions(QUESTION_DATA);
+        expect(service.currentQuestion).toEqual(QUESTION_DATA[0]);
     });
 });
