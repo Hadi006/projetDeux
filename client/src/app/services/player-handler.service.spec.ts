7fa9a2646e739f8cda96d107992e3095034028af
umi2002 <jonathanhoang2002@gmail.com>
2024-01-31 00:47:11 -0500

    Removed useless getter test

--- client/src/app/services/player-handler.service.spec.ts
+++ client/src/app/services/player-handler.service.spec.ts
@@ -17,9 +17,4 @@ describe('PlayerHandlerService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
-
-    it('players should return the correct value', () => {
-        service['playersMap'] = MOCK_PLAYERS;
-        expect(service.players).toEqual(MOCK_PLAYERS);
-    });
 });
