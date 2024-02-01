cc1f193c137e19fe5974e445dc59e3ee6cb71f62
umi2002 <jonathanhoang2002@gmail.com>
2024-01-31 23:15:00 -0500

    Added test for time getter

--- client/src/app/services/game-timers.service.spec.ts
+++ client/src/app/services/game-timers.service.spec.ts
@@ -5,15 +5,19 @@ import { GameTimersService } from './game-timers.service';
 import { TimeService } from './time.service';
 
 describe('GameTimersService', () => {
+    const QUESTION_TIMER_ID = 0;
+    const ANSWER_TIMER_ID = 1;
+
     let service: GameTimersService;
     let timeServiceSpy: jasmine.SpyObj<TimeService>;
     let gameStateServiceSpy: jasmine.SpyObj<GameStateService>;
 
     beforeEach(() => {
         timeServiceSpy = jasmine.createSpyObj('TimeService', ['createTimer', 'startTimer', 'stopTimer', 'getTime', 'setTime']);
+        timeServiceSpy.createTimer.and.returnValues(QUESTION_TIMER_ID, ANSWER_TIMER_ID);
 
         gameStateServiceSpy = jasmine.createSpyObj('GameStateService', ['nextState']);
-        Object.defineProperty(gameStateServiceSpy, 'state', { get: () => 0, configurable: true });
+        Object.defineProperty(gameStateServiceSpy, 'gameState', { get: () => 0, configurable: true });
     });
 
     beforeEach(() => {
@@ -32,5 +36,13 @@ describe('GameTimersService', () => {
 
     it('should create 2 timers', () => {
         expect(timeServiceSpy.createTimer).toHaveBeenCalledTimes(2);
+    });
+
+    it('time getter should return the correct value when game state is ShowQuestion', () => {
+        const time = 10;
+        spyOnProperty(gameStateServiceSpy, 'gameState', 'get').and.returnValue(GameState.ShowQuestion);
+        timeServiceSpy.getTime.and.returnValue(time);
+        expect(service.time).toBe(time);
+        expect(timeServiceSpy.getTime).toHaveBeenCalledWith(QUESTION_TIMER_ID);
     });
 });
