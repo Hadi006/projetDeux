f184a71ccf0ac663f9684b85fdca4f72d8f814ba
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 02:52:25 -0500

    Added player handler service spy

--- client/src/app/components/player/player.component.spec.ts
+++ client/src/app/components/player/player.component.spec.ts
@@ -1,15 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { PlayerHandlerService } from '@app/services/player-handler.service';
 
 import { PlayerComponent } from './player.component';
 
 describe('PlayerComponent', () => {
     let component: PlayerComponent;
     let fixture: ComponentFixture<PlayerComponent>;
+    let playerHandlerServiceSpy: jasmine.SpyObj<PlayerHandlerService>;
 
     beforeEach(() => {
+        playerHandlerServiceSpy = jasmine.createSpyObj<PlayerHandlerService>('PlayerHandlerService', ['createPlayer']);
+    });
+
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [PlayerComponent],
-        });
+            providers: [{ provide: PlayerHandlerService, useValue: playerHandlerServiceSpy }],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
         fixture = TestBed.createComponent(PlayerComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
