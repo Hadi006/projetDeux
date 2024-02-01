72c104fb65c3d6d12b62cfa94697f06c49f65597
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 00:23:50 -0500

    Added question handler spy

--- client/src/app/services/game-state.service.spec.ts
+++ client/src/app/services/game-state.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GameStateService } from './game-state.service';
+import { GameStateService } from '@app/services/game-state.service';
+import { QuestionHandlerService } from '@app/services/question-handler.service';
 
 describe('GameStateService', () => {
     let service: GameStateService;
+    let questionHandlerServiceSpy: jasmine.SpyObj<QuestionHandlerService>;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        questionHandlerServiceSpy = jasmine.createSpyObj<QuestionHandlerService>('QuestionHandlerService', ['currentQuestion']);
+        Object.defineProperty(questionHandlerServiceSpy, 'currentQuestion', {
+            get: () => {
+                return undefined;
+            },
+            configurable: true,
+        });
+    });
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [{ provide: QuestionHandlerService, useValue: questionHandlerServiceSpy }],
+        });
         service = TestBed.inject(GameStateService);
     });
 
