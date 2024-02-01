f219103aae10833cbebe02bd7ab666c928b39808
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 03:11:50 -0500

    Added test for load game data

--- client/src/app/services/game-handler.service.spec.ts
+++ client/src/app/services/game-handler.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GameHandlerService } from '@app/services/game-handler.service';
+import { GameHandlerService, TEST_GAME } from '@app/services/game-handler.service';
 
 describe('GameHandlerService', () => {
     let service: GameHandlerService;
@@ -16,5 +16,11 @@ describe('GameHandlerService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('loadGameData should load the correct game', () => {
+        // TODO - mock the http request
+        service.loadGameData();
+        expect(service.gameData).toEqual(TEST_GAME);
     });
 });
