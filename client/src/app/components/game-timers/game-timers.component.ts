311c0de3a6db047e104c881b1a8265deb366b5d9
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 00:07:26 -0500

    Added OnInit function

--- client/src/app/components/game-timers/game-timers.component.ts
+++ client/src/app/components/game-timers/game-timers.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { GameTimersService } from '@app/services/game-timers.service';
+import { Component, OnInit } from '@angular/core';
+import { GameTimersService, QUESTION_DELAY } from '@app/services/game-timers.service';
 
 @Component({
     selector: 'app-game-timers',
     templateUrl: './game-timers.component.html',
     styleUrls: ['./game-timers.component.scss'],
 })
-export class GameTimersComponent {
+export class GameTimersComponent implements OnInit {
     constructor(private gameTimersService: GameTimersService) {}
 
     get time(): number {
         return this.gameTimersService.time;
+    }
+
+    ngOnInit(): void {
+        this.gameTimersService.startQuestionTimer(QUESTION_DELAY);
     }
 }
