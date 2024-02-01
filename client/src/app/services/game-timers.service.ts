ec9a689ec086e96852f8fca529d4dee03c81aaf7
umi2002 <jonathanhoang2002@gmail.com>
2024-01-31 22:52:39 -0500

    Injected time service

--- client/src/app/services/game-timers.service.ts
+++ client/src/app/services/game-timers.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { TimeService } from '@app/services/time.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class GameTimersService {}
+export class GameTimersService {
+    constructor(private timeService: TimeService) {}
+}
