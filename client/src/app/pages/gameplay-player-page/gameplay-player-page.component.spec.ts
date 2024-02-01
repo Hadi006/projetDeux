0baca400ae3c250478b665422fb0341efc0b828b
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 03:02:19 -0500

    Reset tests

--- client/src/app/pages/gameplay-player-page/gameplay-player-page.component.spec.ts
+++ client/src/app/pages/gameplay-player-page/gameplay-player-page.component.spec.ts
@@ -1,39 +1,20 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { Router } from '@angular/router';
-
 import { GameplayPlayerPageComponent } from '@app/pages/gameplay-player-page/gameplay-player-page.component';
-import { GameHandlerService, GameState } from '@app/services/game-handler.service';
-import { PlayerHandlerService } from '@app/services/player-handler.service';
-import { BehaviorSubject } from 'rxjs';
+import { GameHandlerService } from '@app/services/game-handler.service';
 
 describe('GameplayPlayerPageComponent', () => {
-    const SCORE = 10;
-
     let component: GameplayPlayerPageComponent;
     let fixture: ComponentFixture<GameplayPlayerPageComponent>;
     let gameHandlerServiceSpy: jasmine.SpyObj<GameHandlerService>;
-    let playerHandlerServiceSpy: jasmine.SpyObj<PlayerHandlerService>;
-    let routerSpy: jasmine.SpyObj<Router>;
 
     beforeEach(() => {
-        gameHandlerServiceSpy = jasmine.createSpyObj('GameHandlerService', ['startGame'], {
-            stateSubject: new BehaviorSubject<GameState>(GameState.ShowQuestion),
-        });
-
-        playerHandlerServiceSpy = jasmine.createSpyObj('PlayerHandlerService', ['createPlayer']);
-        playerHandlerServiceSpy.createPlayer.and.returnValue({ score: SCORE, answerNotifier: new BehaviorSubject<boolean[]>([]) });
-
-        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+        gameHandlerServiceSpy = jasmine.createSpyObj('GameHandlerService', ['startGame']);
     });
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [GameplayPlayerPageComponent],
-            providers: [
-                { provide: GameHandlerService, useValue: gameHandlerServiceSpy },
-                { provide: PlayerHandlerService, useValue: playerHandlerServiceSpy },
-                { provide: Router, useValue: routerSpy },
-            ],
+            providers: [{ provide: GameHandlerService, useValue: gameHandlerServiceSpy }],
         }).compileComponents();
     }));
 
@@ -45,37 +26,5 @@ describe('GameplayPlayerPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
-    });
-
-    it('should create a player', () => {
-        expect(playerHandlerServiceSpy.createPlayer).toHaveBeenCalled();
-        expect(component.player).toBeTruthy();
-    });
-
-    it('should set showingAnswer to false when the game state is ShowQuestion', () => {
-        component.gameHandlerService.stateSubject.next(GameState.ShowQuestion);
-        expect(component.showingAnswer).toBeFalse();
-    });
-
-    it('should set showingAnswer to true and update score when the game state is ShowAnswer', () => {
-        component.gameHandlerService.stateSubject.next(GameState.ShowAnswer);
-
-        expect(component.showingAnswer).toBeTrue();
-        expect(component.score).toEqual(SCORE);
-    });
-
-    it('should navigate to the home page when the game state is GameEnded', () => {
-        component.gameHandlerService.stateSubject.next(GameState.GameEnded);
-        expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
-    });
-
-    it('should call gameHandlerService.startGame() when ngOnInit() is called', () => {
-        component.ngOnInit();
-        expect(gameHandlerServiceSpy.startGame).toHaveBeenCalled();
-    });
-
-    it('should unsubscribe from gameStateSubscription when ngOnDestroy() is called', () => {
-        component.ngOnDestroy();
-        expect(component['gameStateSubscription'].closed).toBeTrue();
     });
 });
--- client/src/app/pages/gameplay-player-page/gameplay-player-page.component.ts
+++ client/src/app/pages/gameplay-player-page/gameplay-player-page.component.ts
@@ -1,79 +1,16 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
 import { GameHandlerService } from '@app/services/game-handler.service';
-import { PlayerHandlerService } from '@app/services/player-handler.service';
-import { Subscription } from 'rxjs';
-import { Player } from '@app/interfaces/player';
-import { GameStateService, GameState } from '@app/services/game-state.service';
-import { GameTimersService } from '@app/services/game-timers.service';
 
 @Component({
     selector: 'app-gameplay-player-page',
     templateUrl: './gameplay-player-page.component.html',
     styleUrls: ['./gameplay-player-page.component.scss'],
 })
-export class GameplayPlayerPageComponent implements OnInit, OnDestroy {
+export class GameplayPlayerPageComponent implements OnInit {
     @Input() gameId: number;
 
-    private internalTime: number;
-    private internalPlayer: Player;
-    private internalShowingAnswer: boolean;
-    private internalScore: number;
-    private gameStateSubscription: Subscription;
-
-    constructor(
-        public gameHandlerService: GameHandlerService,
-        private router: Router,
-        private playerHandlerService: PlayerHandlerService,
-        private gameStateService: GameStateService,
-        private gameTimersService: GameTimersService,
-    ) {
-        this.internalPlayer = this.playerHandlerService.createPlayer();
-
-        this.gameStateSubscription = this.gameHandlerService.stateSubject.subscribe((state: GameState) => {
-            switch (state) {
-                case GameState.ShowQuestion:
-                    this.internalShowingAnswer = false;
-                    break;
-                case GameState.ShowAnswer:
-                    this.internalShowingAnswer = true;
-                    this.internalScore = this.internalPlayer.score;
-                    break;
-                case GameState.GameEnded:
-                    this.router.navigate(['/']);
-                    break;
-            }
-        });
-    }
-
-    get time(): number {
-        switch (this.gameStateService.gameState) {
-            case GameState.ShowQuestion:
-                return this.gameTimersService.questionTime;
-            case GameState.ShowAnswer:
-                return this.gameTimersService.answerTime;
-            default:
-                return 0;
-        }
-    }
-
-    get player(): Player | undefined {
-        return this.internalPlayer;
-    }
-
-    get showingAnswer(): boolean | undefined {
-        return this.internalShowingAnswer;
-    }
-
-    get score(): number | undefined {
-        return this.internalScore;
-    }
-
+    constructor(public gameHandlerService: GameHandlerService) {}
     ngOnInit(): void {
         this.gameHandlerService.startGame();
-    }
-
-    ngOnDestroy(): void {
-        this.gameStateSubscription.unsubscribe();
     }
 }
