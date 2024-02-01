55778cc78be84e1e16172766598c57f34bbba8b2
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 01:01:09 -0500

    Revert "Reset tests"
    This reverts commit 94d5d8135514f100ed79ea598091b50f1cc013dc.

--- client/src/app/services/player-handler.service.spec.ts
+++ client/src/app/services/player-handler.service.spec.ts
@@ -1,50 +1,50 @@
-// import { TestBed } from '@angular/core/testing';
-// import { Subject } from 'rxjs';
+import { TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
 
-// import { PlayerHandlerService } from './player-handler.service';
+import { PlayerHandlerService } from './player-handler.service';
 
-// describe('PlayerHandlerService', () => {
-//     let service: PlayerHandlerService;
+describe('PlayerHandlerService', () => {
+    let service: PlayerHandlerService;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({});
-//         service = TestBed.inject(PlayerHandlerService);
-//     });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(PlayerHandlerService);
+    });
 
-//     it('should be created', () => {
-//         expect(service).toBeTruthy();
-//     });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 
-//     it('createPlayer should add a player to the map with the correct id', () => {
-//         const nPlayers = service.nPlayers;
-//         spyOn(service.players, 'set').and.callThrough();
-//         const player = service.createPlayer();
+    it('createPlayer should add a player to the map with the correct id', () => {
+        const nPlayers = service.nPlayers;
+        spyOn(service.players, 'set').and.callThrough();
+        const player = service.createPlayer();
 
-//         expect(service.players.set).toHaveBeenCalledWith(nPlayers, player);
-//     });
+        expect(service.players.set).toHaveBeenCalledWith(nPlayers, player);
+    });
 
-//     it('createPlayer should return a new player and increment nPlayers', () => {
-//         const nPlayers = service.nPlayers;
-//         const player = service.createPlayer();
+    it('createPlayer should return a new player and increment nPlayers', () => {
+        const nPlayers = service.nPlayers;
+        const player = service.createPlayer();
 
-//         expect(player).toEqual({ score: 0, answerNotifier: new Subject<boolean[]>() });
-//         expect(service.nPlayers).toEqual(nPlayers + 1);
-//     });
+        expect(player).toEqual({ score: 0, answerNotifier: new Subject<boolean[]>() });
+        expect(service.nPlayers).toEqual(nPlayers + 1);
+    });
 
-//     it('cleanUp should unsubscribe all players', () => {
-//         const nPlayers = 5;
-//         for (let i = 0; i < nPlayers; i++) {
-//             service.createPlayer();
-//         }
+    it('cleanUp should unsubscribe all players', () => {
+        const nPlayers = 5;
+        for (let i = 0; i < nPlayers; i++) {
+            service.createPlayer();
+        }
 
-//         service.players.forEach((player) => {
-//             spyOn(player.answerNotifier, 'unsubscribe').and.callThrough();
-//         });
+        service.players.forEach((player) => {
+            spyOn(player.answerNotifier, 'unsubscribe').and.callThrough();
+        });
 
-//         service.cleanUp();
+        service.cleanUp();
 
-//         service.players.forEach((player) => {
-//             expect(player.answerNotifier.unsubscribe).toHaveBeenCalled();
-//         });
-//     });
-// });
+        service.players.forEach((player) => {
+            expect(player.answerNotifier.unsubscribe).toHaveBeenCalled();
+        });
+    });
+});
--- client/src/app/services/player-handler.service.ts
+++ client/src/app/services/player-handler.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Player } from '@app/interfaces/player';
-import { QuestionHandlerService } from './question-handler.service';
 
 @Injectable({
     providedIn: 'root',
@@ -8,9 +8,6 @@ import { QuestionHandlerService } from './question-handler.service';
 export class PlayerHandlerService {
     private internalPlayers: Map<number, Player> = new Map<number, Player>();
     private internalNPlayers: number = 0;
-    private internalNAnswered: number = 0;
-
-    constructor(private questionHandlerService: QuestionHandlerService) {}
 
     get players(): Map<number, Player> {
         return this.internalPlayers;
@@ -20,38 +17,19 @@ export class PlayerHandlerService {
         return this.internalNPlayers;
     }
 
-    get nAnswered(): number {
-        return this.internalNAnswered;
-    }
-
-    get allAnswered(): boolean {
-        return this.internalNAnswered >= this.internalNPlayers;
-    }
-
     createPlayer(): Player {
         const player: Player = {
             score: 0,
-            answer: [],
-            answerConfirmed: false,
-            confirmAnswer: this.confirmAnswer.bind(this),
+            answerNotifier: new Subject<boolean[]>(),
         };
         this.internalPlayers.set(this.internalNPlayers++, player);
 
         return player;
     }
 
-    private confirmAnswer(): void {
-        this.internalNAnswered++;
-
-        if (this.allAnswered) {
-            this.updateScores();
-        }
-    }
-
-    private updateScores(): void {
+    cleanUp(): void {
         this.internalPlayers.forEach((player: Player) => {
-            const score = this.questionHandlerService.calculateScore(player.answer);
-            player.score += score;
+            player.answerNotifier.unsubscribe();
         });
     }
 }
