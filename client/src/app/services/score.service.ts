3f5300a08d2471680fc78ae7acdf8c2327fac642
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 16:02:00 -0500

    Moved logic for calculating score to score service

--- client/src/app/services/score.service.ts
+++ client/src/app/services/score.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 import { QuestionHandlerService } from './question-handler.service';
 
+export const GOOD_ANSWER_MULTIPLIER = 1.2;
+
 @Injectable({
     providedIn: 'root',
 })
 export class ScoreService {
     constructor(private questionHandlerService: QuestionHandlerService) {}
+
+    calculateScore(isChecked: boolean[]): number {
+        if (!this.questionHandlerService.currentQuestion) {
+            return 0;
+        }
+
+        const score = this.questionHandlerService.currentQuestion.points * GOOD_ANSWER_MULTIPLIER;
+
+        return this.questionHandlerService.isAnswerCorrect(isChecked) ? score : 0;
+    }
 }
