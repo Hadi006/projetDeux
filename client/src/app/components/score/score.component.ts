244ba4dc70f350a19d2a9b4bc26770b6392a33b6
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 17:36:41 -0500

    Instantiate score service

--- client/src/app/components/score/score.component.ts
+++ client/src/app/components/score/score.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { Player } from '@app/interfaces/player';
+import { ScoreService } from '@app/services/score.service';
 
 @Component({
     selector: 'app-score',
     templateUrl: './score.component.html',
     styleUrls: ['./score.component.scss'],
 })
-export class ScoreComponent {
+export class ScoreComponent implements OnDestroy {
     @Input() player: Player;
+
+    constructor(private scoreService: ScoreService) {}
+
+    ngOnDestroy(): void {
+        this.scoreService.cleanUp();
+    }
 }
