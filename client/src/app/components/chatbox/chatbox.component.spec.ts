a33970660cea7e15f887587ff509cabde71c1ed2
Your Name <mikaelcarrenard@yahoo.ca>
2024-02-01 01:30:40 -0500

    test chatBox

--- client/src/app/components/chatbox/chatbox.component.spec.ts
+++ client/src/app/components/chatbox/chatbox.component.spec.ts
@@ -1,21 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { ChatboxComponent } from './chatbox.component';
 
-describe('ChatboxComponent', () => {
-  let component: ChatboxComponent;
-  let fixture: ComponentFixture<ChatboxComponent>;
+describe('ChatComponent', () => {
+    let component: ChatboxComponent;
+    let fixture: ComponentFixture<ChatboxComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ChatboxComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ChatboxComponent],
+            imports: [FormsModule],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(ChatboxComponent);
+        component = fixture.componentInstance;
     });
-    fixture = TestBed.createComponent(ChatboxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have an empty messages array initially', () => {
+        expect(component.messages.length).toBe(0);
+    });
+
+    it('should add a new message to the messages array', () => {
+        const initialMessageCount = component.messages.length;
+
+        component.newMessage = 'Test message';
+        component.sendMessage();
+
+        expect(component.messages.length).toBe(initialMessageCount + 1);
+    });
 });
