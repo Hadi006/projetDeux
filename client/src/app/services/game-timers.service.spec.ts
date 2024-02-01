102a1e3e813709bffadbd33be521b3964dcaa03c
umi2002 <jonathanhoang2002@gmail.com>
2024-01-31 22:51:31 -0500

    Reset game timers service

--- client/src/app/services/game-timers.service.spec.ts
+++ client/src/app/services/game-timers.service.spec.ts
@@ -4,10 +4,6 @@ import { GameTimersService } from './game-timers.service';
 import { TimeService } from './time.service';
 
 describe('GameTimersService', () => {
-    const TIME_OUT = 10;
-    const QUESTION_ID = 0;
-    const ANSWER_ID = 1;
-
     let service: GameTimersService;
     let timeServiceSpy: jasmine.SpyObj<TimeService>;
 
@@ -24,109 +20,5 @@ describe('GameTimersService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
-    });
-
-    it('get questionTime should call timeService.getTime with the correct id', () => {
-        timeServiceSpy.createTimer.and.returnValue(QUESTION_ID);
-        timeServiceSpy.getTime.and.returnValue(TIME_OUT);
-        service.createQuestionTimer(() => {
-            return;
-        });
-
-        expect(service.questionTime).toEqual(TIME_OUT);
-        expect(timeServiceSpy.getTime).toHaveBeenCalledWith(QUESTION_ID);
-    });
-
-    it('get answerTime should call timeService.getTime with the correct id', () => {
-        timeServiceSpy.createTimer.and.returnValue(ANSWER_ID);
-        timeServiceSpy.getTime.and.returnValue(TIME_OUT);
-        service.createAnswerTimer(() => {
-            return;
-        });
-
-        expect(service.answerTime).toEqual(TIME_OUT);
-        expect(timeServiceSpy.getTime).toHaveBeenCalledWith(ANSWER_ID);
-    });
-
-    it('createQuestionTimer should call createTimer with the correct callback and assign an id to questionTimerId', () => {
-        const callback = () => {
-            return;
-        };
-        timeServiceSpy.createTimer.and.returnValue(QUESTION_ID);
-        service.createQuestionTimer(callback);
-
-        expect(timeServiceSpy.createTimer).toHaveBeenCalledWith(callback);
-        expect(service['questionTimerId']).toEqual(QUESTION_ID);
-    });
-
-    it('createAnswerTimer should call createTimer with the correct callback and assign an id to answerTimerId', () => {
-        const callback = () => {
-            return;
-        };
-        timeServiceSpy.createTimer.and.returnValue(ANSWER_ID);
-        service.createAnswerTimer(callback);
-
-        expect(timeServiceSpy.createTimer).toHaveBeenCalledWith(callback);
-        expect(service['answerTimerId']).toEqual(ANSWER_ID);
-    });
-
-    it('startQuestionTimer should call startTimer with the correct id and time', () => {
-        timeServiceSpy.createTimer.and.returnValue(QUESTION_ID);
-        service.createQuestionTimer(() => {
-            return;
-        });
-        service.startQuestionTimer(TIME_OUT);
-
-        expect(timeServiceSpy.startTimer).toHaveBeenCalledWith(QUESTION_ID, TIME_OUT);
-    });
-
-    it('startAnswerTimer should call startTimer with the correct id and time', () => {
-        timeServiceSpy.createTimer.and.returnValue(ANSWER_ID);
-        service.createAnswerTimer(() => {
-            return;
-        });
-        service.startAnswerTimer(TIME_OUT);
-
-        expect(timeServiceSpy.startTimer).toHaveBeenCalledWith(ANSWER_ID, TIME_OUT);
-    });
-
-    it('stopQuestionTimer should call stopTimer with the correct id', () => {
-        timeServiceSpy.createTimer.and.returnValue(QUESTION_ID);
-        service.createQuestionTimer(() => {
-            return;
-        });
-        service.stopQuestionTimer();
-
-        expect(timeServiceSpy.stopTimer).toHaveBeenCalledWith(QUESTION_ID);
-    });
-
-    it('stopAnswerTimer should call stopTimer with the correct id', () => {
-        timeServiceSpy.createTimer.and.returnValue(ANSWER_ID);
-        service.createAnswerTimer(() => {
-            return;
-        });
-        service.stopAnswerTimer();
-
-        expect(timeServiceSpy.stopTimer).toHaveBeenCalledWith(ANSWER_ID);
-    });
-
-    it('setQuestionTime should call setTime with the correct id and time', () => {
-        timeServiceSpy.createTimer.and.returnValue(QUESTION_ID);
-        service.createQuestionTimer(() => {
-            return;
-        });
-        service.setQuestionTime(TIME_OUT);
-
-        expect(timeServiceSpy.setTime).toHaveBeenCalledWith(QUESTION_ID, TIME_OUT);
-    });
-
-    it('setAnswerTime should call setTime with the correct id and time', () => {
-        timeServiceSpy.createTimer.and.returnValue(ANSWER_ID);
-        service.createAnswerTimer(() => {
-            return;
-        });
-        service.setAnswerTime(TIME_OUT);
-
-        expect(timeServiceSpy.setTime).toHaveBeenCalledWith(ANSWER_ID, TIME_OUT);
     });
 });
--- client/src/app/services/game-timers.service.ts
+++ client/src/app/services/game-timers.service.ts
@@ -1,52 +1,6 @@
 import { Injectable } from '@angular/core';
-import { TimeService } from '@app/services/time.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class GameTimersService {
-    private questionTimerId: number;
-    private answerTimerId: number;
-
-    constructor(private timeService: TimeService) {}
-
-    get questionTime(): number {
-        return this.timeService.getTime(this.questionTimerId);
-    }
-
-    get answerTime(): number {
-        return this.timeService.getTime(this.answerTimerId);
-    }
-
-    createQuestionTimer(questionTimerCallback: () => void): void {
-        this.questionTimerId = this.timeService.createTimer(questionTimerCallback);
-    }
-
-    createAnswerTimer(answerTimerCallback: () => void): void {
-        this.answerTimerId = this.timeService.createTimer(answerTimerCallback);
-    }
-
-    startQuestionTimer(time: number): void {
-        this.timeService.startTimer(this.questionTimerId, time);
-    }
-
-    startAnswerTimer(time: number): void {
-        this.timeService.startTimer(this.answerTimerId, time);
-    }
-
-    stopQuestionTimer(): void {
-        this.timeService.stopTimer(this.questionTimerId);
-    }
-
-    stopAnswerTimer(): void {
-        this.timeService.stopTimer(this.answerTimerId);
-    }
-
-    setQuestionTime(time: number): void {
-        this.timeService.setTime(this.questionTimerId, time);
-    }
-
-    setAnswerTime(time: number): void {
-        this.timeService.setTime(this.answerTimerId, time);
-    }
-}
+export class GameTimersService {}
