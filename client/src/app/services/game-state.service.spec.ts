63aa330280b83c7792161d557c9fbaff5d159d72
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 03:52:06 -0500

    game state service is now only used to contain a single instance of the game state

--- client/src/app/services/game-state.service.spec.ts
+++ client/src/app/services/game-state.service.spec.ts
@@ -1,53 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GameStateService, GameState } from '@app/services/game-state.service';
-import { QuestionHandlerService } from '@app/services/question-handler.service';
-import { QUESTIONS_DATA } from '@app/services/game-handler.service';
+import { GameStateService } from '@app/services/game-state.service';
 
 describe('GameStateService', () => {
     let service: GameStateService;
-    let questionHandlerServiceSpy: jasmine.SpyObj<QuestionHandlerService>;
 
     beforeEach(() => {
-        questionHandlerServiceSpy = jasmine.createSpyObj<QuestionHandlerService>('QuestionHandlerService', ['currentQuestion']);
-        Object.defineProperty(questionHandlerServiceSpy, 'currentQuestion', {
-            get: () => {
-                return undefined;
-            },
-            configurable: true,
-        });
-    });
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [{ provide: QuestionHandlerService, useValue: questionHandlerServiceSpy }],
-        });
+        TestBed.configureTestingModule({});
         service = TestBed.inject(GameStateService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
-    });
-
-    it('nextState should set state to GameEnded if currentQuestion is undefined', () => {
-        service.nextState();
-        expect(service.gameState).toBe(GameState.GameEnded);
-    });
-
-    it('nextState should set the states in the correct order if currentQuestion is defined', () => {
-        spyOnProperty(questionHandlerServiceSpy, 'currentQuestion', 'get').and.returnValue(QUESTIONS_DATA[0]);
-        service.nextState();
-        expect(service.gameState).toBe(GameState.ShowQuestion);
-        service.nextState();
-        expect(service.gameState).toBe(GameState.ShowAnswer);
-        service.nextState();
-        expect(service.gameState).toBe(GameState.ShowQuestion);
-    });
-
-    it('nextState should set state to GameEnded if current state is GameEnded', () => {
-        service.nextState();
-        spyOnProperty(questionHandlerServiceSpy, 'currentQuestion', 'get').and.returnValue(QUESTIONS_DATA[0]);
-        service.nextState();
-        expect(service.gameState).toBe(GameState.GameEnded);
     });
 });
--- client/src/app/services/game-state.service.ts
+++ client/src/app/services/game-state.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { QuestionHandlerService } from '@app/services/question-handler.service';
 
 export enum GameState {
     ShowQuestion = 0,
@@ -11,32 +10,5 @@ export enum GameState {
     providedIn: 'root',
 })
 export class GameStateService {
-    private internalGameState: GameState;
-
-    constructor(private questionHandlerService: QuestionHandlerService) {}
-
-    get gameState(): GameState {
-        return this.internalGameState;
-    }
-
-    nextState(): void {
-        if (!this.questionHandlerService.currentQuestion) {
-            this.internalGameState = GameState.GameEnded;
-        } else {
-            switch (this.internalGameState) {
-                case GameState.ShowQuestion:
-                    this.internalGameState = GameState.ShowAnswer;
-                    break;
-                case GameState.ShowAnswer:
-                    this.internalGameState = GameState.ShowQuestion;
-                    break;
-                case GameState.GameEnded:
-                    this.internalGameState = GameState.GameEnded;
-                    break;
-                default:
-                    this.internalGameState = GameState.ShowQuestion;
-                    break;
-            }
-        }
-    }
+    gameState: GameState = GameState.ShowQuestion;
 }
