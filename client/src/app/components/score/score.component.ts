911d27e1ec7bb52d939cb0027156121f29dc1aab
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 16:17:51 -0500

    Added player input and score display

--- client/src/app/components/score/score.component.ts
+++ client/src/app/components/score/score.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Player } from '@app/interfaces/player';
 
 @Component({
     selector: 'app-score',
     templateUrl: './score.component.html',
     styleUrls: ['./score.component.scss'],
 })
-export class ScoreComponent {}
-
+export class ScoreComponent {
+    @Input() player: Player;
+    score = 0;
+}
