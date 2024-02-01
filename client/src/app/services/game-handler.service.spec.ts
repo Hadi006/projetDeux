817985f07a2fc0eb392255e3ffa05f92c8e1c279
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 03:06:21 -0500

    Reset tests

--- client/src/app/services/game-handler.service.spec.ts
+++ client/src/app/services/game-handler.service.spec.ts
@@ -1,118 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { Player } from '@app/interfaces/player';
 
-import { GameHandlerService, GameState, TEST_GAME } from '@app/services/game-handler.service';
-import { PlayerHandlerService } from '@app/services/player-handler.service';
-import { Subject } from 'rxjs';
-import { GameTimersService } from './game-timers.service';
-import { QuestionHandlerService } from './question-handler.service';
+import { GameHandlerService } from '@app/services/game-handler.service';
 
 describe('GameHandlerService', () => {
     let service: GameHandlerService;
-    let gameTimerServiceSpy: jasmine.SpyObj<GameTimersService>;
-    let playerHandlerServiceSpy: jasmine.SpyObj<PlayerHandlerService>;
-    let questionHandlerServiceSpy: jasmine.SpyObj<QuestionHandlerService>;
 
-    beforeEach(() => {
-        gameTimerServiceSpy = jasmine.createSpyObj('GameTimersService', [
-            'createQuestionTimer',
-            'createAnswerTimer',
-            'startQuestionTimer',
-            'startAnswerTimer',
-            'getQuestionTime',
-            'getAnswerTime',
-            'setQuestionTime',
-        ]);
-        Object.defineProperty(gameTimerServiceSpy, 'questionTime', {
-            get: () => {
-                return 0;
-            },
-            configurable: true,
-        });
-        Object.defineProperty(gameTimerServiceSpy, 'answerTime', {
-            get: () => {
-                return 0;
-            },
-            configurable: true,
-        });
-
-        playerHandlerServiceSpy = playerHandlerServiceSpy = jasmine.createSpyObj<PlayerHandlerService>('PlayerHandlerService', ['createPlayer'], {
-            players: new Map<number, Player>(),
-            nPlayers: 0,
-        });
-
-        questionHandlerServiceSpy = jasmine.createSpyObj<QuestionHandlerService>('QuestionHandlerService', [
-            'setQuestions',
-            'nextQuestion',
-            'calculateScore',
-        ]);
-        Object.defineProperty(questionHandlerServiceSpy, 'currentQuestion', {
-            get: () => {
-                return undefined;
-            },
-            configurable: true,
-        });
-    });
+    beforeEach(() => {});
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [
-                { provide: GameTimersService, useValue: gameTimerServiceSpy },
-                { provide: PlayerHandlerService, useValue: playerHandlerServiceSpy },
-                { provide: QuestionHandlerService, useValue: questionHandlerServiceSpy },
-            ],
+            providers: [],
         });
         service = TestBed.inject(GameHandlerService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
-    });
-
-    it('get time should return the correct value when game state is ShowQuestion', () => {
-        const TIME = 10;
-        spyOnProperty(gameTimerServiceSpy, 'questionTime', 'get').and.returnValue(TIME);
-        questionHandlerServiceSpy.setQuestions.and.callThrough();
-        questionHandlerServiceSpy.setQuestions(TEST_GAME.questions);
-        service.updateGameState(GameState.ShowQuestion);
-
-        expect(service.time).toEqual(TIME);
-    });
-
-    it('get time should call getAnswerTime when the game state is ShowAnswer and return its value', () => {
-        const TIME = 10;
-        spyOnProperty(gameTimerServiceSpy, 'answerTime', 'get').and.returnValue(TIME);
-        questionHandlerServiceSpy.setQuestions.and.callThrough();
-        questionHandlerServiceSpy.setQuestions(TEST_GAME.questions);
-        service.updateGameState(GameState.ShowAnswer);
-
-        expect(service.time).toEqual(TIME);
-    });
-
-    it('get time should return 0 when the game state is GameEnded', () => {
-        service.startGame();
-
-        expect(service.time).toEqual(0);
-    });
-
-    it('get time should return undefined when the game state is not recognized', () => {
-        expect(service.time).toBeUndefined();
-    });
-
-    it('startGame should create a subscription for each players answerNotifier', () => {
-        const N_PLAYERS = 3;
-        for (let i = 0; i < N_PLAYERS; i++) {
-            const player: Player = {
-                score: 0,
-                answerNotifier: new Subject<boolean[]>(),
-            };
-            spyOn(player.answerNotifier, 'subscribe')
-            playerHandlerServiceSpy.players.set(i, player);
-        }
-        service.startGame();
-
-        playerHandlerServiceSpy.players.forEach((player: Player) => {
-            expect(player.answerNotifier.subscribe).toHaveBeenCalled();
-        });
     });
 });
