21b321a5e8a242d071612deefd621ec722e1a224
umi2002 <jonathanhoang2002@gmail.com>
2024-02-01 00:29:50 -0500

    Added test for nextState

--- client/src/app/services/game-state.service.spec.ts
+++ client/src/app/services/game-state.service.spec.ts
@@ -1,7 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GameStateService } from '@app/services/game-state.service';
+import { GameStateService, GameState } from '@app/services/game-state.service';
 import { QuestionHandlerService } from '@app/services/question-handler.service';
+import { QUESTIONS_DATA } from '@app/services/game-handler.service';
 
 describe('GameStateService', () => {
     let service: GameStateService;
@@ -26,5 +27,15 @@ describe('GameStateService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('nextState should set the states in the correct order if currentQuestion is defined', () => {
+        spyOnProperty(questionHandlerServiceSpy, 'currentQuestion', 'get').and.returnValue(QUESTIONS_DATA[0]);
+        service.nextState();
+        expect(service.gameState).toBe(GameState.ShowQuestion);
+        service.nextState();
+        expect(service.gameState).toBe(GameState.ShowAnswer);
+        service.nextState();
+        expect(service.gameState).toBe(GameState.ShowQuestion);
     });
 });
