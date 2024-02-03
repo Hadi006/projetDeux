cf3bf2c25712616c366c6e83fb7e02ba972c87a5
umi2002 <jonathanhoang2002@gmail.com>
2024-02-02 21:11:09 -0500

    Modularized before Each

--- client/src/app/components/chatbox/chatbox.component.spec.ts
+++ client/src/app/components/chatbox/chatbox.component.spec.ts
@@ -1,19 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { ChatboxComponent, MAX_MESSAGE_LENGTH } from './chatbox.component';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ChatboxComponent } from './chatbox.component';
+import { ChatService } from '@app/services/chat.service';
 
 describe('ChatComponent', () => {
     let component: ChatboxComponent;
     let fixture: ComponentFixture<ChatboxComponent>;
+    let chatServiceSpy: jasmine.SpyObj<ChatService>;
 
     beforeEach(() => {
+        chatServiceSpy = jasmine.createSpyObj('ChatService', ['sendMessage']);
+    });
+
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [ChatboxComponent],
-            imports: [FormsModule],
+            providers: [{ provide: ChatService, useValue: chatServiceSpy }],
         }).compileComponents();
+    }));
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(ChatboxComponent);
         component = fixture.componentInstance;
+        fixture.detectChanges();
     });
 
     it('should create the component', () => {
