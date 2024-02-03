22607e9301aafbaacae5ca8870d463942e296aa9
umi2002 <jonathanhoang2002@gmail.com>
2024-02-02 21:01:28 -0500

    Moved constant to chat service. Injected service in component

--- client/src/app/services/chat.service.ts
+++ client/src/app/services/chat.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 
+export const MAX_MESSAGE_LENGTH = 200;
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ChatService {
-
-  constructor() { }
+    constructor() {}
 }
