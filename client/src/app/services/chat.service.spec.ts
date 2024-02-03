e131ab4713160280603635e75f0a36d103ec0cae
umi2002 <jonathanhoang2002@gmail.com>
2024-02-02 21:15:52 -0500

    Added test for sending empty message

--- client/src/app/services/chat.service.spec.ts
+++ client/src/app/services/chat.service.spec.ts
@@ -3,14 +3,19 @@ import { TestBed } from '@angular/core/testing';
 import { ChatService } from './chat.service';
 
 describe('ChatService', () => {
-  let service: ChatService;
+    let service: ChatService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ChatService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ChatService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should do nothing when sendMessage is called with an empty string', () => {
+        service.sendMessage('');
+        expect(service.messages.length).toBe(0);
+    });
 });
