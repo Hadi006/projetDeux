38695a53d214ef8b68b412cf3e2a9eab5ce6f21b
umi2002 <jonathanhoang2002@gmail.com>
2024-02-06 09:47:27 -0500

    Injected socket service and added lobby data attribute

--- client/src/app/services/lobby.service.ts
+++ client/src/app/services/lobby.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SocketService } from '@app/services/socket.service';
+import { LobbyData } from '@common/lobby-data';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LobbyService {
     lobbyId: number;
+    lobbyData: LobbyData;
 
-    constructor(private readonly route: ActivatedRoute) {
+    constructor(
+        private readonly route: ActivatedRoute,
+        private readonly socketService: SocketService,
+    ) {
         this.lobbyId = this.route.snapshot.params.id;
     }
 }
