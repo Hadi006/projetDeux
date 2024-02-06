54601343925b4e7e293204bb1249748a1a6d096a
umi2002 <jonathanhoang2002@gmail.com>
2024-02-06 10:10:23 -0500

    Cleaned up unused code

--- client/src/app/services/lobby.service.ts
+++ client/src/app/services/lobby.service.ts
@@ -1,19 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { SocketService } from '@app/services/socket.service';
 import { LobbyData } from '@common/lobby-data';
-
-const testPlayersData = [
-    { id: 1, name: 'Player 1' },
-    { id: 2, name: 'Player 2' },
-];
-const testGameData = { id: 1, name: 'Math' };
-const testLobbyData: LobbyData = {
-    id: 1,
-    players: testPlayersData,
-    game: testGameData,
-    started: false,
-};
 
 @Injectable({
     providedIn: 'root',
