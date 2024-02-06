e4d2e4e48bf7c6700dca73272fcb1bf3a72614dd
umi2002 <jonathanhoang2002@gmail.com>
2024-02-06 11:04:57 -0500

    Added test for time getter

--- client/src/app/components/game-count-down/game-count-down.component.spec.ts
+++ client/src/app/components/game-count-down/game-count-down.component.spec.ts
@@ -4,12 +4,15 @@ import { GameCountDownComponent } from './game-count-down.component';
 import { TimeService } from '@app/services/time.service';
 
 describe('GameCountDownComponent', () => {
+    const TEST_ID = 1;
+
     let component: GameCountDownComponent;
     let fixture: ComponentFixture<GameCountDownComponent>;
     let timeServiceSpy: jasmine.SpyObj<TimeService>;
 
     beforeEach(() => {
         timeServiceSpy = jasmine.createSpyObj('TimeService', ['createTimerById', 'getTimeById', 'startTimerById']);
+        timeServiceSpy.createTimerById.and.returnValue(TEST_ID);
     });
 
     beforeEach(() => {
@@ -24,5 +27,12 @@ describe('GameCountDownComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('time getter should call getTimeById', () => {
+        const time = 10;
+        timeServiceSpy.getTimeById.and.returnValue(time);
+        expect(component.time).toBe(timeServiceSpy.getTimeById(time));
+        expect(timeServiceSpy.getTimeById).toHaveBeenCalledWith(TEST_ID);
     });
 });
