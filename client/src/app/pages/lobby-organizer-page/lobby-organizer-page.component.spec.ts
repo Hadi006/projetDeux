f4d786684b4799e2c9b9e8070fad9e43d32f37f9
umi2002 <jonathanhoang2002@gmail.com>
2024-02-06 10:17:07 -0500

    Added service spies

--- client/src/app/pages/lobby-organizer-page/lobby-organizer-page.component.spec.ts
+++ client/src/app/pages/lobby-organizer-page/lobby-organizer-page.component.spec.ts
@@ -7,10 +7,21 @@ import { LobbyService } from '@app/services/lobby.service';
 describe('LobbyOrganizerPageComponent', () => {
     let component: LobbyOrganizerPageComponent;
     let fixture: ComponentFixture<LobbyOrganizerPageComponent>;
+    let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>;
+    let lobbyServiceSpy: jasmine.SpyObj<LobbyService>;
+
+    beforeEach(() => {
+        activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', ['snapshot']);
+        lobbyServiceSpy = jasmine.createSpyObj('LobbyService', ['subscribeToLobbyDataById']);
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [LobbyOrganizerPageComponent],
+            providers: [
+                { provide: ActivatedRoute, useValue: activatedRouteSpy },
+                { provide: LobbyService, useValue: lobbyServiceSpy },
+            ],
         });
         fixture = TestBed.createComponent(LobbyOrganizerPageComponent);
         component = fixture.componentInstance;
