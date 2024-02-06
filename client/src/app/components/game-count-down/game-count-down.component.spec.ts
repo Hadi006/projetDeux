dea3036ceba688f3caa6b9622a76ff0b42bde0ef
umi2002 <jonathanhoang2002@gmail.com>
2024-02-06 11:02:17 -0500

    Tests setup

--- client/src/app/components/game-count-down/game-count-down.component.spec.ts
+++ client/src/app/components/game-count-down/game-count-down.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameCountDownComponent } from './game-count-down.component';
+import { TimeService } from '@app/services/time.service';
 
 describe('GameCountDownComponent', () => {
     let component: GameCountDownComponent;
     let fixture: ComponentFixture<GameCountDownComponent>;
+    let timeServiceSpy: jasmine.SpyObj<TimeService>;
+
+    beforeEach(() => {
+        timeServiceSpy = jasmine.createSpyObj('TimeService', ['createTimerById', 'getTimeById', 'startTimerById']);
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [GameCountDownComponent],
+            providers: [{ provide: TimeService, useValue: timeServiceSpy }],
         });
         fixture = TestBed.createComponent(GameCountDownComponent);
         component = fixture.componentInstance;
