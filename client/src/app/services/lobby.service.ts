c6e6017375e88d8998a544b54752ae6831223c92
umi2002 <jonathanhoang2002@gmail.com>
2024-02-06 10:01:56 -0500

    Added test data. Lobby data can now also be filtered by id

--- client/src/app/services/lobby.service.ts
+++ client/src/app/services/lobby.service.ts
@@ -3,24 +3,32 @@ import { ActivatedRoute } from '@angular/router';
 import { SocketService } from '@app/services/socket.service';
 import { LobbyData } from '@common/lobby-data';
 
+const testPlayersData = [
+    { id: 1, name: 'Player 1' },
+    { id: 2, name: 'Player 2' },
+];
+const testGameData = { id: 1, name: 'Math' };
+const testLobbyData: LobbyData = {
+    id: 1,
+    players: testPlayersData,
+    game: testGameData,
+    started: false,
+};
+
 @Injectable({
     providedIn: 'root',
 })
 export class LobbyService {
-    lobbyId: number;
     lobbyData: LobbyData;
 
     constructor(
-        private readonly route: ActivatedRoute,
         private readonly socketService: SocketService,
     ) {
-        this.lobbyId = this.route.snapshot.params.id;
-        this.subscribeToLobbyData();
     }
 
-    private subscribeToLobbyData() {
+    subscribeToLobbyDataById(id: number) {
         this.socketService.filteredDataByType<LobbyData>('lobbyData').subscribe((lobbyData) => {
-            if (lobbyData.id === this.lobbyId) {
+            if (id === lobbyData.id) {
                 this.lobbyData = lobbyData;
             }
         });
