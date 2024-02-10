881b1072b2e6b17d000876d28789599755f5d311
umi2002 <jonathanhoang2002@gmail.com>
2024-02-10 11:52:08 -0500

    Removed page for questions

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GameChoicePageComponent } from '@app/pages/game-choice-page/game-choice-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
-import { QuestionsPageComponent } from '@app/pages/questions-page/questions-page.component';
 import { DescriptionPanelComponent } from './components/description-panel/description-panel.component';
 
 /**
@@ -28,7 +27,6 @@ import { DescriptionPanelComponent } from './components/description-panel/descri
         PlayAreaComponent,
         SidebarComponent,
         GameChoicePageComponent,
-        QuestionsPageComponent,
         DescriptionPanelComponent,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule],
