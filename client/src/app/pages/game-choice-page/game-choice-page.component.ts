af2ac67e803c503990a6e8e145ac11806ce097e3
umi2002 <jonathanhoang2002@gmail.com>
2024-02-10 10:58:51 -0500

    Cleaned up comments

--- client/src/app/pages/game-choice-page/game-choice-page.component.ts
+++ client/src/app/pages/game-choice-page/game-choice-page.component.ts
@@ -7,15 +7,12 @@ import { Router } from '@angular/router';
     styleUrls: ['./game-choice-page.component.scss'],
 })
 export class GameChoicePageComponent {
-    // Liste des jeux disponibles
     games = ['Math', 'Science', 'Programmation', 'Histoire', 'Physique', 'Mode aleatoire'];
 
-    // Jeu sélectionné
     chosenGame: string | null = null;
 
     constructor(private router: Router) {}
 
-    // Méthode pour gérer la sélection d'un jeu
     chooseGame(game: string) {
         this.chosenGame = game;
     }
@@ -29,8 +26,6 @@ export class GameChoicePageComponent {
 
     testGame() {
         if (this.chosenGame) {
-            // Navigue vers la page de test du jeu sélectionné
-            // Assurez-vous que la route 'test/:jeu' est configurée dans votre module de routage
             this.router.navigate(['/test', this.chosenGame]);
         }
     }
