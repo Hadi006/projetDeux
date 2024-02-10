57e81372cad1831a1d0501b53ef9e96d95a8adb4
BaiWuLi <libaiwu59678@gmail.com>
2024-02-10 18:33:17 -0500

    fix lint

--- client/src/app/pages/admin-login-page/admin-login-page.component.spec.ts
+++ client/src/app/pages/admin-login-page/admin-login-page.component.spec.ts
@@ -1,21 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@app/services/auth.service';
+import { of } from 'rxjs';
+import { AdminLoginPageComponent } from './admin-login-page.component';
 
-import { AdminPageComponent } from './admin-login-page.component';
+describe('AdminLoginPageComponent', () => {
+    let component: AdminLoginPageComponent;
+    let fixture: ComponentFixture<AdminLoginPageComponent>;
+    let authServiceSpy: jasmine.SpyObj<AuthService>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
-describe('AdminPageComponent', () => {
-  let component: AdminPageComponent;
-  let fixture: ComponentFixture<AdminPageComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AdminPageComponent]
+    beforeEach(() => {
+        authServiceSpy = jasmine.createSpyObj('AuthService', ['checkAuthentication']);
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     });
-    fixture = TestBed.createComponent(AdminPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [AdminLoginPageComponent],
+            imports: [ReactiveFormsModule],
+            providers: [{ provide: AuthService, useValue: authServiceSpy }, { provide: Router, useValue: routerSpy }, FormBuilder],
+        });
+        fixture = TestBed.createComponent(AdminLoginPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('login should reset form', () => {
+        component.loginForm.setValue({ password: 'password' });
+        authServiceSpy.checkAuthentication.and.returnValue(of(true));
+        component.login();
+        expect(component.loginForm.value.password).toBe(null);
+        component.loginForm.setValue({ password: null });
+        component.login();
+        expect(component.loginForm.value.password).toBe(null);
+    });
+
+    it('login should navigate to admin quizzes if authentificated', () => {
+        component.loginForm.setValue({ password: 'password' });
+        authServiceSpy.checkAuthentication.and.returnValue(of(true));
+        component.login();
+        expect(authServiceSpy.checkAuthentication).toHaveBeenCalledWith('password');
+        expect(routerSpy.navigate).toHaveBeenCalledWith(['home/admin/quizzes']);
+    });
+
+    it('login should not navigate to admin quizzes if not authentificated', () => {
+        component.loginForm.setValue({ password: 'password' });
+        authServiceSpy.checkAuthentication.and.returnValue(of(false));
+        component.login();
+        expect(authServiceSpy.checkAuthentication).toHaveBeenCalledWith('password');
+        expect(routerSpy.navigate).not.toHaveBeenCalled();
+    });
 });
