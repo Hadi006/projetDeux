f752066ec6e7f8bb90d1a8c871b6c9d60fc6e243
umi2002 <jonathanhoang2002@gmail.com>
2024-02-10 14:38:07 -0500

    Injected dependency for tests

--- client/src/app/pages/lobby-organizer-page/lobby-organizer-page.component.spec.ts
+++ client/src/app/pages/lobby-organizer-page/lobby-organizer-page.component.spec.ts
@@ -2,20 +2,26 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LobbyOrganizerPageComponent, TEST_LOBBY_DATA } from '@app/pages/lobby-organizer-page/lobby-organizer-page.component';
 import { LobbyService } from '@app/services/lobby.service';
+import { GameHandlerService, TEST_GAME } from '@app/services/game-handler.service';
 
 describe('LobbyOrganizerPageComponent', () => {
     let component: LobbyOrganizerPageComponent;
     let fixture: ComponentFixture<LobbyOrganizerPageComponent>;
     let lobbyServiceSpy: jasmine.SpyObj<LobbyService>;
+    let gameHandlerServiceSpy: jasmine.SpyObj<GameHandlerService>;
 
     beforeEach(() => {
         lobbyServiceSpy = jasmine.createSpyObj('LobbyService', ['subscribeLobbyToServer']);
+        gameHandlerServiceSpy = jasmine.createSpyObj('GameHandlerService', [], {
+            gameData: TEST_GAME,
+        });
     });
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [LobbyOrganizerPageComponent],
-            providers: [{ provide: LobbyService, useValue: lobbyServiceSpy }],
+            providers: [{ provide: LobbyService, useValue: lobbyServiceSpy },
+                        { provide: GameHandlerService, useValue: gameHandlerServiceSpy }],
         }).compileComponents();
     }));
 
