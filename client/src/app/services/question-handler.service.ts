3a4578df23c39614114baaaccb3914f58def7e85
umi2002 <jonathanhoang2002@gmail.com>
2024-02-11 14:23:02 -0500

    Formatting

--- client/src/app/services/question-handler.service.ts
+++ client/src/app/services/question-handler.service.ts
@@ -29,7 +29,7 @@ export class QuestionHandlerService implements OnDestroy {
     }
 
     get currentAnswers(): Answer[] {
-return this.currentQuestion?.choices.filter((choice) => choice.isCorrect) || [];
+        return this.currentQuestion?.choices.filter((choice) => choice.isCorrect) || [];
     }
 
     get nQuestions(): number {
