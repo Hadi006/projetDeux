fce8f1ff5e0d0c4ba8ffe37fe96d6e1b626a2c3b
BaiWuLi <libaiwu59678@gmail.com>
2024-02-11 08:12:41 -0500

    add validateToken

--- server/app/services/auth.service.ts
+++ server/app/services/auth.service.ts
@@ -10,4 +10,15 @@ export class AuthService {
 
         return Boolean(RESULT && 'password' in RESULT && RESULT.password === password);
     }
+
+    async validateToken(token: unknown): Promise<boolean> {
+        if (!this.isObjectToken(token)) {
+            return false;
+        }
+
+        await this.database.delete('tokens', { expirationDate: { $lt: Date.now() } });
+        const RESULT = await this.database.get('tokens', { id: token.id });
+
+        return RESULT && RESULT.length > 0;
+    }
 }
