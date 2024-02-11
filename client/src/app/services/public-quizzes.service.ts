0ee1c459b5297725e88df66b2840f575f1a4cc9e
umi2002 <jonathanhoang2002@gmail.com>
2024-02-11 12:31:41 -0500

    Added subject to inform subscribers of visible quizzes

--- client/src/app/services/public-quizzes.service.ts
+++ client/src/app/services/public-quizzes.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { CommunicationService } from '@app/services/communication.service';
+import { Quiz } from '@common/quiz';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class PublicQuizzesService {
+    readonly quizzes$: Subject<Quiz[]>;
 
-  constructor() { }
+    constructor(private http: CommunicationService) {}
 }
