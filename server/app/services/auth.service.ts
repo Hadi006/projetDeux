e82101b7515029e1d6caf36faec90b3f974d1594
BaiWuLi <libaiwu59678@gmail.com>
2024-02-11 08:12:27 -0500

    add validatePassword

--- server/app/services/auth.service.ts
+++ server/app/services/auth.service.ts
@@ -4,4 +4,10 @@ import { DatabaseService } from './database.service';
 @Service()
 export class AuthService {
     constructor(private database: DatabaseService) {}
+
+    async validatePassword(password: unknown): Promise<boolean> {
+        const RESULT = (await this.database.get<object>('password'))[0];
+
+        return Boolean(RESULT && 'password' in RESULT && RESULT.password === password);
+    }
 }
