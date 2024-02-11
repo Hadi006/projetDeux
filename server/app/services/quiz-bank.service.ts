589328623eda70865aded1eefc5241f3ccf7e8ee
BaiWuLi <libaiwu59678@gmail.com>
2024-02-11 09:52:40 -0500

    add exportQuiz

--- server/app/services/quiz-bank.service.ts
+++ server/app/services/quiz-bank.service.ts
@@ -10,4 +10,15 @@ export class QuizBankService {
         const RESULT = await this.database.get('quizzes');
         return RESULT as Quiz[];
     }
+
+    async exportQuiz(quizId: string): Promise<Quiz | undefined> {
+        const RESULT = (await this.database.get('quizzes', { id: quizId }, { visible: 0 })) as Quiz[];
+        let quiz = RESULT[0];
+        quiz = { ...quiz, questions: [] };
+        RESULT[0]?.questions.forEach((question) => {
+            delete question.id;
+            quiz.questions.push(question);
+        });
+        return RESULT[0] as Quiz;
+    }
 }
