c6a579ee58e52c679ddfbd80269140248c883adb
umi2002 <jonathanhoang2002@gmail.com>
2024-02-11 00:27:39 -0500

    Unnested code from if blocks

--- client/src/app/pages/game-choice-page/game-choice-page.component.ts
+++ client/src/app/pages/game-choice-page/game-choice-page.component.ts
@@ -22,22 +22,25 @@ export class GameChoicePageComponent {
     }
 
     previewQuestions() {
-        if (this.chosenGame) {
-            this.router.navigate(['/questions', this.chosenGame]);
+        if (!this.chosenGame) {
+            return;
         }
+        this.router.navigate(['/questions', this.chosenGame]);
     }
 
     startGame() {
-        if (this.chosenGame) {
+        if (!this.chosenGame) {
+            return;
+        }
         this.gameHandlerService.loadGameData();
         this.router.navigate(['lobby']);
-        }
     }
 
     testGame() {
-        if (this.chosenGame) {
-            this.gameHandlerService.loadGameData();
-            this.router.navigate(['/play']);
+        if (!this.chosenGame) {
+            return;
         }
+        this.gameHandlerService.loadGameData();
+        this.router.navigate(['/play']);
     }
 }
