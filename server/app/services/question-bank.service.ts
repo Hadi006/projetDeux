2a074aa7e420e39f5db4757254edf3e6a1fbdf22
BaiWuLi <libaiwu59678@gmail.com>
2024-02-11 11:05:35 -0500

    add getQuestions

--- server/app/services/question-bank.service.ts
+++ server/app/services/question-bank.service.ts
@@ -1,7 +1,13 @@
 import { DatabaseService } from '@app/services/database.service';
+import { Question } from '@common/quiz';
 import { Service } from 'typedi';
 
 @Service()
 export class QuestionBankService {
     constructor(private database: DatabaseService) {}
+
+    async getQuestions(): Promise<Question[]> {
+        const QUESTIONS = await this.database.get<Question>('questions');
+        return QUESTIONS;
+    }
 }
