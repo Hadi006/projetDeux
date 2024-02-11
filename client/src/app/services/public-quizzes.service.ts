ffee2751ce6545c71fd1a5e63928d0d2995835e5
umi2002 <jonathanhoang2002@gmail.com>
2024-02-11 12:33:38 -0500

    Moved fetching visible quiz logic to public service

--- client/src/app/services/public-quizzes.service.ts
+++ client/src/app/services/public-quizzes.service.ts
@@ -1,3 +1,4 @@
+import { HttpResponse, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CommunicationService } from '@app/services/communication.service';
 import { Quiz } from '@common/quiz';
@@ -10,4 +11,13 @@ export class PublicQuizzesService {
     readonly quizzes$: Subject<Quiz[]>;
 
     constructor(private http: CommunicationService) {}
+
+    fetchVisibleQuizzes() {
+        this.http.get<Quiz[]>('quizzes/visible').subscribe((response: HttpResponse<Quiz[]>) => {
+            if (!response.body || response.status !== HttpStatusCode.Ok || !Array.isArray(response.body)) {
+                return;
+            }
+            this.quizzes$.next(response.body);
+        });
+    }
 }
