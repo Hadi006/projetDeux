8b520925d78aefa2d9b42a68502cb7f51359ff1f
umi2002 <jonathanhoang2002@gmail.com>
2024-02-11 13:16:17 -0500

    Converted game strings to quiz objects

--- client/src/app/components/description-panel/description-panel.component.ts
+++ client/src/app/components/description-panel/description-panel.component.ts
@@ -1,32 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { Question, Quiz } from '@common/quiz';
+import { Component, Input } from '@angular/core';
+import { Quiz } from '@common/quiz';
 
 @Component({
     selector: 'app-description-panel',
     templateUrl: './description-panel.component.html',
     styleUrls: ['./description-panel.component.scss'],
 })
-export class DescriptionPanelComponent implements OnChanges {
+export class DescriptionPanelComponent {
     @Input() selectedQuiz: Quiz | null = null;
-    description: string = '';
-    duration: string = '';
-    questions: Question[] = [];
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if (changes.selectedGame) {
-            this.updateGameInfo(this.selectedQuiz);
-        }
-    }
-
-    private updateGameInfo(quiz: Quiz | null) {
-        if (quiz) {
-            this.description = quiz.description;
-            this.duration = quiz.duration.toString() + ' seconds';
-            this.questions = quiz.questions;
-        } else {
-            this.description = 'Sélectionnez un jeu pour voir sa description, sa durée et ses questions.';
-            this.duration = '';
-            this.questions = [];
-        }
-    }
 }
