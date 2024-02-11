65de499c9cb866f058faa9f9b476d5f3b487f142
umi2002 <jonathanhoang2002@gmail.com>
2024-02-11 11:55:45 -0500

    Replaced communication service with admin quizzes service

--- client/src/app/pages/game-choice-page/game-choice-page.component.ts
+++ client/src/app/pages/game-choice-page/game-choice-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommunicationService } from '@app/services/communication.service';
+import { AdminQuizzesService } from '@app/services/admin-quizzes.service';
 import { GameHandlerService } from '@app/services/game-handler.service';
 
 @Component({
@@ -14,7 +14,7 @@ export class GameChoicePageComponent {
     chosenGame: string | null = null;
 
     constructor(
-        private communicationService: CommunicationService,
+        private adminQuizzesService: AdminQuizzesService,
         private router: Router,
         private gameHandlerService: GameHandlerService,
     ) {}
