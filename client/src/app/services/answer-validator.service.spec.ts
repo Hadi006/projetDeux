67c46214f5b51dc627b455207661a30e18222d98
umi2002 <jonathanhoang2002@gmail.com>
2024-02-11 23:10:26 -0500

    Added dependencies for tests

--- client/src/app/services/answer-validator.service.spec.ts
+++ client/src/app/services/answer-validator.service.spec.ts
@@ -1,16 +1,26 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { AnswerValidatorService } from './answer-validator.service';
+import { CommunicationService } from './communication.service';
 
 describe('AnswerValidatorService', () => {
-  let service: AnswerValidatorService;
+    let service: AnswerValidatorService;
+    let communicationServiceSpy: jasmine.SpyObj<CommunicationService>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AnswerValidatorService);
-  });
+    beforeEach(() => {
+        communicationServiceSpy = jasmine.createSpyObj('CommunicationService', ['post']);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [{ provide: CommunicationService, useValue: communicationServiceSpy }],
+        });
+        service = TestBed.inject(AnswerValidatorService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
