538d8fef6770cbab81cc8fa7e33754e80036f414
umi2002 <jonathanhoang2002@gmail.com>
2024-02-11 23:21:32 -0500

    Added test for validating an answer

--- client/src/app/services/answer-validator.service.spec.ts
+++ client/src/app/services/answer-validator.service.spec.ts
@@ -1,5 +1,7 @@
+import { HttpResponse, HttpStatusCode } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { AnswerValidatorService } from './answer-validator.service';
 import { CommunicationService } from './communication.service';
@@ -22,5 +24,17 @@ describe('AnswerValidatorService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should validate an answer', () => {
+        const questionId = '1234';
+        const answer = [true, false, true, false];
+        const expectedResponse = true;
+        communicationServiceSpy.post.and.returnValue(of(new HttpResponse({ status: HttpStatusCode.Ok, body: expectedResponse })));
+
+        service.validateAnswer(questionId, answer).subscribe((response) => {
+            expect(communicationServiceSpy.post).toHaveBeenCalledWith(`/api/quiz/${questionId}/answer`, answer);
+            expect(response).toBe(expectedResponse);
+        });
     });
 });
--- client/src/app/services/answer-validator.service.ts
+++ client/src/app/services/answer-validator.service.ts
@@ -1,3 +1,4 @@
+import { HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CommunicationService } from '@app/services/communication.service';
 import { Observable, map } from 'rxjs';
@@ -11,6 +12,10 @@ export class AnswerValidatorService {
     validateAnswer(questionId: string, answer: boolean[]): Observable<boolean> {
         return this.communicationService.post<boolean>(`/api/quiz/${questionId}/answer`, answer).pipe(
             map((response) => {
+                if (response.status !== HttpStatusCode.Ok) {
+                    return false;
+                }
+
                 return response.body || false;
             }),
         );
