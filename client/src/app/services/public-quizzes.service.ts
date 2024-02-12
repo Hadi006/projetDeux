a2c69da975a1cef6d214c4911c3ce9d43340a188
umi2002 <jonathanhoang2002@gmail.com>
2024-02-12 09:38:58 -0500

    Fixed linting

--- client/src/app/services/public-quizzes.service.ts
+++ client/src/app/services/public-quizzes.service.ts
@@ -43,7 +43,7 @@ export class PublicQuizzesService {
         this.dialog.open(AlertComponent, {
             data: {
                 title: 'Erreur',
-                message: 'Le questionnaire n\'est plus disponible',
+                message: "Le questionnaire n'est plus disponible",
             },
         });
     }
