388c11d5b5c23549dc94350e1264a6f3a279e759
BaiWuLi <libaiwu59678@gmail.com>
2024-02-13 21:31:26 -0500

    implement resubmit with prompt

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -37,6 +37,15 @@ export class AdminPageComponent implements OnInit {
                 return;
             }
 
+            if (response.errorLog.includes('title must be unique')) {
+                const newTitle = prompt('Please enter a new title for the quiz');
+                if (!newTitle) {
+                    return;
+                }
+                this.adminService.submitQuiz(response.quiz, newTitle).subscribe();
+                return;
+            }
+
             this.dialog.open(AlertComponent, { data: { message: response.errorLog } });
         });
     }
