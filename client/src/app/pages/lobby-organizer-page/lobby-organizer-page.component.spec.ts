5ef85ca52ce57c3d2c800b623b6caddf2084673c
umi2002 <jonathanhoang2002@gmail.com>
2024-03-07 18:58:08 -0500

    Remove unused service

--- client/src/app/pages/lobby-organizer-page/lobby-organizer-page.component.spec.ts
+++ client/src/app/pages/lobby-organizer-page/lobby-organizer-page.component.spec.ts
@@ -1,41 +1,22 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { GameCountDownComponent } from '@app/components/game-count-down/game-count-down.component';
 import { LobbyOrganizerPageComponent } from '@app/pages/lobby-organizer-page/lobby-organizer-page.component';
-import { GameHandlerService } from '@app/services/game-handler.service';
 import { LobbyService } from '@app/services/lobby.service';
-import { Quiz } from '@common/quiz';
 import { TEST_LOBBY_DATA } from '@common/constant';
 
 describe('LobbyOrganizerPageComponent', () => {
-    const TEST_QUIZ: Quiz = {
-        id: '0',
-        title: 'Math',
-        visible: true,
-        description: 'Math quiz',
-        duration: 10,
-        lastModification: new Date(),
-        questions: [],
-    };
-
     let component: LobbyOrganizerPageComponent;
     let fixture: ComponentFixture<LobbyOrganizerPageComponent>;
     let lobbyServiceSpy: jasmine.SpyObj<LobbyService>;
-    let gameHandlerServiceSpy: jasmine.SpyObj<GameHandlerService>;
 
     beforeEach(() => {
         lobbyServiceSpy = {} as jasmine.SpyObj<LobbyService>;
-        gameHandlerServiceSpy = jasmine.createSpyObj('GameHandlerService', [], {
-            quizData: TEST_QUIZ,
-        });
     });
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [LobbyOrganizerPageComponent, GameCountDownComponent],
-            providers: [
-                { provide: LobbyService, useValue: lobbyServiceSpy },
-                { provide: GameHandlerService, useValue: gameHandlerServiceSpy },
-            ],
+            providers: [{ provide: LobbyService, useValue: lobbyServiceSpy }],
         }).compileComponents();
     }));
 
