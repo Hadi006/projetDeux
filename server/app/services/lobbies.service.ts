2fd521730362114af0f5bfdc0e18f120794c9b59
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 15:29:31 -0500

    Added method to get all lobbies

--- server/app/services/lobbies.service.ts
+++ server/app/services/lobbies.service.ts
@@ -1,7 +1,12 @@
 import { Service } from 'typedi';
 import { DatabaseService } from './database.service';
+import { LobbyData } from '@common/lobby-data';
 
 @Service()
 export class LobbiesService {
     constructor(private database: DatabaseService) {}
+
+    async getLobbies(): Promise<LobbyData[]> {
+        return await this.database.get<LobbyData>('lobbies');
+    }
 }
