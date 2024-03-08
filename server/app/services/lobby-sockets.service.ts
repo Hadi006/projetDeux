fc4104a3f78d85d0dc6e4702c5c49881a4f0a2c5
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 17:50:30 -0500

    Added socket event listener to create a lobby

--- server/app/services/lobby-sockets.service.ts
+++ server/app/services/lobby-sockets.service.ts
@@ -1,16 +1,27 @@
 import { Server as SocketIOServer, Socket } from 'socket.io';
 import { Server as HTTPServer } from 'http';
+import { LobbyData } from '@common/lobby-data';
+import { LobbiesService } from './lobbies.service';
 
 export class LobbySocketsService {
     private io: SocketIOServer;
 
-    constructor(server: HTTPServer) {
+    constructor(
+        private lobbiesService: LobbiesService,
+        server: HTTPServer,
+    ) {
         this.io = new SocketIOServer(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
     }
 
     handleSockets(): void {
         this.io.on('connection', (socket: Socket) => {
             this.disconnect(socket);
+        });
+    }
+
+    private createLobby(socket: Socket): void {
+        socket.on('create-lobby', (lobby: LobbyData) => {
+            this.lobbiesService.addLobby(lobby);
         });
     }
 
