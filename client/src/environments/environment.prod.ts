6cd5991d36734e6075b01c1e44f35769d7fc52d5
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 13:17:09 -0500

    Fixed server url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-3-98-129-202.ca-central-1.compute.amazonaws.com:3000/api',
+    serverUrl: 'http://ec2-3-98-129-202.ca-central-1.compute.amazonaws.com:3000',
 };
