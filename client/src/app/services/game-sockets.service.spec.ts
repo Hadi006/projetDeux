f767c1a17dbafa6bf2cacf08f3b1d960e3330ffb
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 14:56:36 -0500

    Added setup for tests

--- client/src/app/services/game-sockets.service.spec.ts
+++ client/src/app/services/game-sockets.service.spec.ts
@@ -1,13 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
 
 import { GameSocketsService } from './game-sockets.service';
 
 describe('GameSocketsService', () => {
     let service: GameSocketsService;
+    let socketMock;
+    let ioSpy: jasmine.Spy;
+
+    beforeEach(() => {
+        socketMock = {
+            emit: jasmine.createSpy('emit'),
+            disconnect: jasmine.createSpy('disconnect'),
+        };
+
+        ioSpy = jasmine.createSpy('io').and.returnValue(socketMock);
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
+    });
+
+    beforeEach(() => {
         service = TestBed.inject(GameSocketsService);
+        service['socket'] = ioSpy(environment.serverUrl, { transports: ['websocket'], upgrade: false });
     });
 
     it('should be created', () => {
