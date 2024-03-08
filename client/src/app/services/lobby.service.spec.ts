041db7fa70d935a971d45ae6dd3253d71234f100
umi2002 <jonathanhoang2002@gmail.com>
2024-03-07 19:12:10 -0500

    Added game handler provider

--- client/src/app/services/lobby.service.spec.ts
+++ client/src/app/services/lobby.service.spec.ts
@@ -1,9 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { LobbyService } from '@app/services/lobby.service';
 import { SocketService } from '@app/services/socket.service';
 import { LobbyData } from '@common/lobby-data';
 import { Quiz } from '@common/quiz';
 import { of } from 'rxjs';
+import { GameHandlerService } from '@app/services/game-handler.service';
 
 describe('LobbyService', () => {
     const quizData: Quiz = {
@@ -33,14 +35,21 @@ describe('LobbyService', () => {
 
     let service: LobbyService;
     let socketServiceSpy: jasmine.SpyObj<SocketService>;
+    let gameHandlerServiceSpy: jasmine.SpyObj<GameHandlerService>;
 
     beforeEach(() => {
         socketServiceSpy = jasmine.createSpyObj('SocketService', ['filteredDataByType']);
+        gameHandlerServiceSpy = {} as jasmine.SpyObj<GameHandlerService>;
+        Object.defineProperty(gameHandlerServiceSpy, 'quizData', { value: quizData });
     });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [{ provide: SocketService, useValue: socketServiceSpy }],
+            imports: [HttpClientTestingModule],
+            providers: [
+                { provide: SocketService, useValue: socketServiceSpy },
+                { provide: GameHandlerService, useValue: gameHandlerServiceSpy },
+            ],
         });
         service = TestBed.inject(LobbyService);
     });
