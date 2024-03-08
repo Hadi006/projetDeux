89357101ab818a306058a5ff7cffe381028f9587
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 13:32:45 -0500

    Formatting

--- client/src/app/services/game-sockets.service.spec.ts
+++ client/src/app/services/game-sockets.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { GameSocketsService } from './game-sockets.service';
 
 describe('GameSocketsService', () => {
-  let service: GameSocketsService;
+    let service: GameSocketsService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GameSocketsService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(GameSocketsService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
