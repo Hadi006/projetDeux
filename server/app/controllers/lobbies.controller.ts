58d49556e72814e42f37df626700d78e6cc27099
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 15:44:02 -0500

    Added service for setting up routes for the lobby db api

---
+++ server/app/controllers/lobbies.controller.ts
@@ -0,0 +1,6 @@
+import { LobbiesService } from './lobbies.service';
+import { LobbyData } from '@common/lobby-data';
+import { Service } from 'typedi';
+
+@Service()
+export class LobbyController {}
