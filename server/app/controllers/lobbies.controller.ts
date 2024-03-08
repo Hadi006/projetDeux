e44fb8e54e3e37197b49d762bd78ecb97ea88ba1
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 15:45:34 -0500

    Basic creation of router

--- server/app/controllers/lobbies.controller.ts
+++ server/app/controllers/lobbies.controller.ts
@@ -1,6 +1,17 @@
 import { LobbiesService } from './lobbies.service';
 import { LobbyData } from '@common/lobby-data';
+import { Router } from 'express';
 import { Service } from 'typedi';
 
 @Service()
-export class LobbyController {}
+export class LobbyController {
+    router: Router;
+
+    constructor(private readonly lobbiesService: LobbiesService) {
+        this.configureRouter();
+    }
+
+    private configureRouter() {
+        this.router = Router();
+    }
+}
