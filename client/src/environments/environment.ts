bbe6c81091355f2ac4acf56686c5907bff3bac9f
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 13:19:25 -0500

    Fixed url for non production environment

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    serverUrl: 'http://ec2-3-98-129-202.ca-central-1.compute.amazonaws.com:3000',
+    serverUrl: 'http://localhost:3000/api',
 };
 
 /*
