2a2032b37519e016eba421f1fbb9d6998d4fb53d
umi2002 <jonathanhoang2002@gmail.com>
2024-03-09 14:00:34 -0500

    Added generic methods for web socket handling

--- client/src/app/services/web-socket.service.ts
+++ client/src/app/services/web-socket.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class WebSocketService {
+    private socket: Socket;
 
-  constructor() { }
+    connect() {
+        this.socket = io(environment.serverUrl, { transports: ['websocket'], upgrade: false });
+    }
+
+    disconnect() {
+        this.socket.disconnect();
+    }
+
+    onEvent<T>(event: string, action: (data: T) => void) {
+        this.socket.on(event, action);
+    }
+
+    emit<T>(event: string, data?: T, callback?: (data: unknown) => void) {
+        this.socket.emit(event, data, callback);
+    }
 }
