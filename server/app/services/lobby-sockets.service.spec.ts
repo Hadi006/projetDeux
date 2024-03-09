01e5c6b4ddee52f401ded3d0c5535a836495d00f
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 22:35:40 -0500

    Corrected tests

--- server/app/services/lobby-sockets.service.spec.ts
+++ server/app/services/lobby-sockets.service.spec.ts
@@ -1,12 +1,13 @@
 import { Server } from '@app/server';
 import { LobbySocketsService } from '@app/services/lobby-sockets.service';
 import { expect } from 'chai';
-import { SinonStubbedInstance, createStubInstance, restore, stub } from 'sinon';
+import { SinonStubbedInstance, createStubInstance, restore /* , stub */ } from 'sinon';
 import { io as ioClient, Socket } from 'socket.io-client';
 import { Container } from 'typedi';
 import { LobbiesService } from './lobbies.service';
-import { LobbyData } from '@common/lobby-data';
 import { Acknowledgment } from '@common/acknowledgment';
+import { Quiz } from '@common/quiz';
+import { LobbyData } from '@common/lobby-data';
 
 describe('LobbySocketsService', () => {
     let service: LobbySocketsService;
@@ -15,10 +16,22 @@ describe('LobbySocketsService', () => {
     let clientSocket: Socket;
 
     const urlString = 'http://localhost:3000';
+
+    const testQuiz: Quiz = {
+        id: '1',
+        title: 'Math',
+        visible: true,
+        description: 'Math quiz',
+        duration: 5,
+        lastModification: new Date(),
+        questions: [],
+    };
+
     const testLobby: LobbyData = {
         id: '1',
         players: [],
         started: false,
+        quiz: { ...testQuiz },
     };
 
     beforeEach(async () => {
@@ -28,7 +41,7 @@ describe('LobbySocketsService', () => {
         server.init();
         service = server['lobbySocketsService'];
         clientSocket = ioClient(urlString);
-        stub(console, 'log');
+        // stub(console, 'log');
     });
 
     afterEach(() => {
@@ -38,19 +51,10 @@ describe('LobbySocketsService', () => {
     });
 
     it('should create a lobby', (done) => {
-        lobbiesServiceStub.addLobby.resolves(true);
-        clientSocket.emit('create-lobby', testLobby, (ack: Acknowledgment) => {
-            expect(ack.success).to.equal(true);
-            expect(lobbiesServiceStub.addLobby.calledWith(testLobby)).to.equal(true);
-            done();
-        });
-    });
-
-    it('should not create a lobby', (done) => {
-        lobbiesServiceStub.addLobby.resolves(false);
-        clientSocket.emit('create-lobby', testLobby, (ack: Acknowledgment) => {
-            expect(ack.success).to.equal(false);
-            expect(lobbiesServiceStub.addLobby.calledWith(testLobby)).to.equal(true);
+        lobbiesServiceStub.createLobby.resolves(testLobby);
+        clientSocket.emit('create-lobby', testQuiz, (ack: LobbyData) => {
+            expect(ack.quiz).to.deep.equal({ ...testQuiz, lastModification: testQuiz.lastModification.toISOString() });
+            expect(lobbiesServiceStub.createLobby.called).to.equal(true);
             done();
         });
     });
