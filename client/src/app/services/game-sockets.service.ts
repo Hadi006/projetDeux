05202a4ded2958e8091b744de3f58201131c9da9
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 20:08:44 -0500

    Convert async and promise to observable

--- client/src/app/services/game-sockets.service.ts
+++ client/src/app/services/game-sockets.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
 import { Acknowledgment } from '@common/acknowledgment';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -17,17 +18,18 @@ export class GameSocketsService {
         this.socket.disconnect();
     }
 
-    async createLobby(lobbyId: string): Promise<boolean> {
-        let result = false;
-
-        this.socket.emit('create-lobby', lobbyId, (ack: Acknowledgment) => {
-            if (ack.success) {
-                this.joinLobby(lobbyId);
-                result = true;
-            }
+    createLobby(lobbyId: string): Observable<boolean> {
+        return new Observable<boolean>((subscriber) => {
+            this.socket.emit('create-lobby', lobbyId, (ack: Acknowledgment) => {
+                if (ack.success) {
+                    this.joinLobby(lobbyId);
+                    subscriber.next(true);
+                } else {
+                    subscriber.next(false);
+                }
+                subscriber.complete();
+            });
         });
-
-        return result;
     }
 
     joinLobby(roomId: string) {
