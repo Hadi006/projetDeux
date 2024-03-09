d606d5cec8ee0d034018920f9810942ca32b89f8
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 23:56:47 -0500

    Adjusted tests

--- client/src/app/services/game-sockets.service.spec.ts
+++ client/src/app/services/game-sockets.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-import { Acknowledgment } from '@common/acknowledgment';
 import { NEW_LOBBY } from '@common/constant';
+import { LobbyData } from '@common/lobby-data';
+import { Quiz } from '@common/quiz';
 import { Socket } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
 
@@ -8,6 +9,26 @@ import { GameSocketsService } from './game-sockets.service';
 
 describe('GameSocketsService', () => {
     const LOBBY_ID = 'lobby-id';
+    const TEST_QUIZ: Quiz = {
+        id: '1',
+        title: 'Test Quiz',
+        visible: true,
+        description: 'Test Quiz Description',
+        duration: 10,
+        lastModification: new Date(),
+        questions: [
+            {
+                id: '1',
+                text: 'Question 1',
+                type: 'QCM',
+                points: 10,
+                choices: [
+                    { text: 'Answer 1', isCorrect: true },
+                    { text: 'Answer 2', isCorrect: false },
+                ],
+            },
+        ],
+    };
 
     let service: GameSocketsService;
     let socketSpy: jasmine.SpyObj<Socket>;
@@ -40,52 +61,37 @@ describe('GameSocketsService', () => {
     it('should create a lobby and join it', (done) => {
         spyOn(service, 'joinLobby');
 
-        socketSpy.emit.and.callFake((event: string, lobbyId: string, callback: (ack: Acknowledgment) => void) => {
-            callback({ success: true });
+        socketSpy.emit.and.callFake((event: string, lobbyId: string, callback: (lobbyData: LobbyData) => void) => {
+            callback({ ...NEW_LOBBY, id: LOBBY_ID });
             return socketSpy;
         });
 
-        service.createLobby({ ...NEW_LOBBY, id: LOBBY_ID }).subscribe((success) => {
-            expect(success).toBeTrue();
+        service.createLobby(TEST_QUIZ).subscribe((lobbyData: LobbyData) => {
+            expect(lobbyData).toEqual({ ...NEW_LOBBY, id: LOBBY_ID });
             expect(service.joinLobby).toHaveBeenCalledWith(LOBBY_ID);
             done();
         });
     });
 
-    it('should not create a lobby', (done) => {
-        spyOn(service, 'joinLobby');
-
-        socketSpy.emit.and.callFake((event: string, lobbyId: string, callback: (ack: Acknowledgment) => void) => {
-            callback({ success: false });
-            return socketSpy;
-        });
-
-        service.createLobby({ ...NEW_LOBBY, id: LOBBY_ID }).subscribe((success) => {
-            expect(success).toBeFalse();
-            expect(service.joinLobby).not.toHaveBeenCalled();
-            done();
-        });
-    });
-
     it('should join a lobby', () => {
-        socketSpy.emit.and.callFake((event: string, roomId: string, callback: (ack: Acknowledgment) => void) => {
-            callback({ success: true });
+        socketSpy.emit.and.callFake((event: string, roomId: string, callback: (errorMsg: string) => void) => {
+            callback('');
             return socketSpy;
         });
 
-        service.joinLobby(LOBBY_ID).subscribe((success) => {
-            expect(success).toBeTrue();
+        service.joinLobby(LOBBY_ID).subscribe((errorMsg) => {
+            expect(errorMsg).toBe('');
         });
     });
 
     it('should not join a lobby', () => {
-        socketSpy.emit.and.callFake((event: string, roomId: string, callback: (ack: Acknowledgment) => void) => {
-            callback({ success: false });
+        socketSpy.emit.and.callFake((event: string, roomId: string, callback: (errorMsg: string) => void) => {
+            callback('Error message');
             return socketSpy;
         });
 
-        service.joinLobby(LOBBY_ID).subscribe((success) => {
-            expect(success).toBeFalse();
+        service.joinLobby(LOBBY_ID).subscribe((errorMsg) => {
+            expect(errorMsg).toBe('Error message');
         });
     });
 });
