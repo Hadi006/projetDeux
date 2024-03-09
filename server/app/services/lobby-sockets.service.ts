a966592e7434694134d8eba5171143fc007b8c09
umi2002 <jonathanhoang2002@gmail.com>
2024-03-08 19:25:28 -0500

    Removed unnecessary decorator

--- server/app/services/lobby-sockets.service.ts
+++ server/app/services/lobby-sockets.service.ts
@@ -2,13 +2,12 @@ import { Server as SocketIOServer, Socket } from 'socket.io';
 import { Server as HTTPServer } from 'http';
 import { LobbyData } from '@common/lobby-data';
 import { LobbiesService } from './lobbies.service';
-import { Inject } from 'typedi';
 
 export class LobbySocketsService {
     private sio: SocketIOServer;
 
     constructor(
-        @Inject(() => LobbiesService) private lobbiesService: LobbiesService,
+        private lobbiesService: LobbiesService,
         server: HTTPServer,
     ) {
         this.sio = new SocketIOServer(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
