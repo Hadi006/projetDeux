97925cbcc9a0f73cc5f3c3efd61f85d90c0b228b
umi2002 <jonathanhoang2002@gmail.com>
2024-03-10 10:48:36 -0400

    Adjusted tests

--- client/src/app/components/game-count-down/game-count-down.component.spec.ts
+++ client/src/app/components/game-count-down/game-count-down.component.spec.ts
@@ -1,7 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { TimeService } from '@app/services/time.service';
 import { GameCountDownComponent } from './game-count-down.component';
 import { START_GAME_COUNTDOWN } from '@common/constant';
+import { HostService } from '@app/services/host.service';
 
 describe('GameCountDownComponent', () => {
     const TEST_ID = 1;
@@ -9,17 +10,25 @@ describe('GameCountDownComponent', () => {
     let component: GameCountDownComponent;
     let fixture: ComponentFixture<GameCountDownComponent>;
     let timeServiceSpy: jasmine.SpyObj<TimeService>;
+    let hostServiceSpy: jasmine.SpyObj<HostService>;
 
     beforeEach(() => {
         timeServiceSpy = jasmine.createSpyObj('TimeService', ['createTimerById', 'getTimeById', 'startTimerById']);
         timeServiceSpy.createTimerById.and.returnValue(TEST_ID);
+        hostServiceSpy = jasmine.createSpyObj('HostService', ['startGame']);
     });
 
-    beforeEach(() => {
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [GameCountDownComponent],
-            providers: [{ provide: TimeService, useValue: timeServiceSpy }],
+            providers: [
+                { provide: TimeService, useValue: timeServiceSpy },
+                { provide: HostService, useValue: hostServiceSpy },
+            ],
         });
+    }));
+
+    beforeEach(() => {
         fixture = TestBed.createComponent(GameCountDownComponent);
         component = fixture.componentInstance;
         component.countdownTime = START_GAME_COUNTDOWN;
@@ -32,6 +41,7 @@ describe('GameCountDownComponent', () => {
 
     it('should call createTimerById on TimeService', () => {
         expect(timeServiceSpy.createTimerById).toHaveBeenCalled();
+        expect(timeServiceSpy.createTimerById.calls.first().args[0]).toEqual(jasmine.any(Function));
     });
 
     it('ngOnInit should call startTimerById', () => {
