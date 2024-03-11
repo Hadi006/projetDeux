60fe03085eafe48cd3796a3431e409f06462a412
RajabFawaz <90001583+RajabFawaz@users.noreply.github.com>
2024-03-11 13:06:54 -0400

    test done for chat service

--- client/src/app/services/chat.service.spec.ts
+++ client/src/app/services/chat.service.spec.ts
@@ -1,20 +1,18 @@
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { MAX_MESSAGE_LENGTH } from '@common/constant';
-import { io, Socket } from 'socket.io-client';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { ChatService } from './chat.service';
 
 describe('ChatService', () => {
     let service: ChatService;
-    let socket: Socket;
+    const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [SocketIoModule.forRoot(config)],
+            providers: [ChatService],
+        });
         service = TestBed.inject(ChatService);
-        socket = io();
-    });
-
-    afterEach(() => {
-        socket.disconnect(); // Disconnect the mock socket after each test
     });
 
     it('should be created', () => {
@@ -29,7 +27,6 @@ describe('ChatService', () => {
         service.sendMessage('');
         expect(service.messages.length).toBe(0);
     });
-
     it('should do nothing when sendMessage is called with a string longer than 200 characters', () => {
         service.sendMessage('a'.repeat(MAX_MESSAGE_LENGTH + 1));
         expect(service.messages.length).toBe(0);
@@ -39,39 +36,6 @@ describe('ChatService', () => {
         service.sendMessage('   ');
         expect(service.messages.length).toBe(0);
     });
-
-    it('should add a message to the messages array when sendMessage is called with a valid message', () => {
-        // Arrange
-        const service = TestBed.inject(ChatService);
-        const initialMessagesLength = service.messages.length; // Get the initial length of messages array
-
-        // Act
-        service.sendMessage('test message');
-
-        // Assert
-        const updatedMessagesLength = service.messages.length; // Get the updated length of messages array
-        expect(updatedMessagesLength).toBe(initialMessagesLength + 1); // Expect the length to increase by 1
-        // expect(service.messages[updatedMessagesLength - 1]).toBe('test message'); // Expect the last message to be 'test message'
-    });
-
-    it('should add received message to messages array', fakeAsync(() => {
-        const message = { text: 'Received message', timestamp: new Date() };
-        socket.emit('message-received', message); // Simulate receiving a message from the server
-        tick(); // Advance the fakeAsync clock to trigger asynchronous operation
-        expect(service.messages.length).toBe(1);
-        expect(service.messages[0]).toEqual(message);
-    }));
-
-    it('should emit messagesSubject after sending a message', fakeAsync(() => {
-        let subjectEmitted = false;
-        service.messagesSubjectGetter.subscribe(() => {
-            subjectEmitted = true;
-        });
-
-        service.sendMessage('New message');
-        tick(); // Advance the fakeAsync clock to trigger asynchronous operation
-        expect(subjectEmitted).toBe(true);
-    }));
 
     it('should validate message correctly', () => {
         // Arrange
@@ -84,11 +48,20 @@ describe('ChatService', () => {
         expect(chatService['validateMessage']('a'.repeat(MAX_MESSAGE_LENGTH + 1))).toBe(false); // Message exceeding max length should return false
     });
 
-    it('should handle unsuccessful message sending', () => {
-        // To test unsuccessful message sending, you may mock the socket.emit method to throw an error
-        spyOn(socket, 'emit').and.throwError('Socket error');
-        service.sendMessage('Test message');
-        expect(socket.emit).toHaveBeenCalled();
-        expect(service.messages.length).toBe(0);
+    // it('should add received message to internal messages', (done) => {
+    //     const testMessage = { text: 'Test Message', timestamp: new Date() };
+    //     service.messagesSubjectGetter.subscribe(() => {
+    //         expect(service.messages.length).toBe(1);
+    //         expect(service.messages[0]).toEqual(testMessage);
+    //         done();
+    //     });
+    //     (service as any).socket.emit('message-received', testMessage);
+    // });
+
+    it('should send valid messages through the socket', () => {
+        spyOn((service as any).socket, 'emit');
+        const newMessage = 'New valid message';
+        service.sendMessage(newMessage);
+        expect((service as any).socket.emit).toHaveBeenCalledWith('new-message', jasmine.any(Object));
     });
 });
