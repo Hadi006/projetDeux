fb6bb5099f23eeb2fb107f111f6721fc27940d12
umi2002 <jonathanhoang2002@gmail.com>
2024-03-14 19:47:16 -0400

    Added leave game logic to player waiting room

--- client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.ts
+++ client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PlayerService } from '@app/services/player.service';
 
 @Component({
-  selector: 'app-waiting-room-player-page',
-  templateUrl: './waiting-room-player-page.component.html',
-  styleUrls: ['./waiting-room-player-page.component.scss']
+    selector: 'app-waiting-room-player-page',
+    templateUrl: './waiting-room-player-page.component.html',
+    styleUrls: ['./waiting-room-player-page.component.scss'],
 })
 export class WaitingRoomPlayerPageComponent {
+    constructor(
+        private playerService: PlayerService,
+        private router: Router,
+    ) {}
 
+    leaveGame() {
+        this.playerService.cleanUp();
+        this.router.navigate(['/']);
+    }
 }
