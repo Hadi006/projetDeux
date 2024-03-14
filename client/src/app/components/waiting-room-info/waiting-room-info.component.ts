0bb125a96aa05bb7724e1836e508de22c748187f
umi2002 <jonathanhoang2002@gmail.com>
2024-03-14 19:33:38 -0400

    Moved common logic from waiting room page to component

--- client/src/app/components/waiting-room-info/waiting-room-info.component.ts
+++ client/src/app/components/waiting-room-info/waiting-room-info.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Game } from '@common/game';
 
 @Component({
-  selector: 'app-waiting-room-info',
-  templateUrl: './waiting-room-info.component.html',
-  styleUrls: ['./waiting-room-info.component.scss']
+    selector: 'app-waiting-room-info',
+    templateUrl: './waiting-room-info.component.html',
+    styleUrls: ['./waiting-room-info.component.scss'],
 })
 export class WaitingRoomInfoComponent {
-
+    @Input() game: Game;
+    @Input() leaveGame: () => void;
 }
