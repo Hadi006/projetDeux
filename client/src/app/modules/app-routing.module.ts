6a2efff5f110592b37eaa0f70a61c7c6fe43d3db
umi2002 <jonathanhoang2002@gmail.com>
2024-03-14 19:16:54 -0400

    Added routing for join room page component

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -8,11 +8,13 @@ import { GameChoicePageComponent } from '@app/pages/game-choice-page/game-choice
 import { TestPageComponent } from '@app/pages/test-page/test-page.component';
 import { HomePageComponent } from '@app/pages/home-page/home-page.component';
 import { WaitingRoomHostPageComponent } from '@app/pages/waiting-room-host-page/waiting-room-host-page.component';
+import { JoinRoomPageComponent } from '@app/pages/join-room-page/join-room-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
     { path: 'home/create-game', component: GameChoicePageComponent },
+    { path: 'home/join-game', component: JoinRoomPageComponent },
     { path: 'home/admin/login', component: AdminLoginPageComponent },
     { path: 'home/admin/quizzes', component: AdminPageComponent, canActivate: [authGuard] },
     { path: 'home/admin/quizzes/quiz', component: CreateQuizPageComponent, canActivate: [authGuard] },
