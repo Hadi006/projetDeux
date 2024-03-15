dbc8cba70ca66f068e2327aefae35395e8c22c98
umi2002 <jonathanhoang2002@gmail.com>
2024-03-15 09:36:48 -0400

    Added subscription in player waiting room to navigate when game starts

--- client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.ts
+++ client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { PlayerService } from '@app/services/player.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-waiting-room-player-page',
     templateUrl: './waiting-room-player-page.component.html',
     styleUrls: ['./waiting-room-player-page.component.scss'],
 })
-export class WaitingRoomPlayerPageComponent {
+export class WaitingRoomPlayerPageComponent implements OnDestroy {
+    private startGameSubscription: Subscription;
+
     constructor(
         private playerService: PlayerService,
         private router: Router,
-    ) {}
+    ) {
+        this.startGameSubscription = this.playerService.startGameSubject.subscribe(() => {
+            router.navigate(['game-player']);
+        });
+    }
 
     get pin() {
         return this.playerService.pin;
@@ -29,5 +36,9 @@ export class WaitingRoomPlayerPageComponent {
         this.playerService.leaveGame();
         this.playerService.cleanUp();
         this.router.navigate(['/']);
+    }
+
+    ngOnDestroy() {
+        this.startGameSubscription.unsubscribe();
     }
 }
