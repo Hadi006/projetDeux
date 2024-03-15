aa85e105cb6039c0964ff5ccf970c5ca8bda19bc
umi2002 <jonathanhoang2002@gmail.com>
2024-03-14 23:30:17 -0400

    Formatting

--- client/src/app/pages/join-room-page/join-room-page.component.spec.ts
+++ client/src/app/pages/join-room-page/join-room-page.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { JoinRoomPageComponent } from './join-room-page.component';
 
 describe('JoinRoomPageComponent', () => {
-  let component: JoinRoomPageComponent;
-  let fixture: ComponentFixture<JoinRoomPageComponent>;
+    let component: JoinRoomPageComponent;
+    let fixture: ComponentFixture<JoinRoomPageComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [JoinRoomPageComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [JoinRoomPageComponent],
+        });
+        fixture = TestBed.createComponent(JoinRoomPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(JoinRoomPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
