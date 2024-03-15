e65e1010aface8ad20cf93409172671fe3ee9459
umi2002 <jonathanhoang2002@gmail.com>
2024-03-15 10:08:02 -0400

    Game count down now keeps track of the countdown itself

--- client/src/app/components/game-count-down/game-count-down.component.ts
+++ client/src/app/components/game-count-down/game-count-down.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { TimeService } from '@app/services/time.service';
+import { START_GAME_COUNTDOWN } from '@common/constant';
 
 @Component({
     selector: 'app-game-count-down',
@@ -7,5 +9,14 @@ import { Component, Input } from '@angular/core';
 })
 export class GameCountDownComponent {
     @Input() gameName: string;
-    @Input() time: number;
+
+    private timerId: number;
+
+    constructor(private timeService: TimeService) {
+        this.timerId = this.timeService.createTimerById();
+    }
+
+    getTime() {
+        return this.timeService.getTimeById(this.timerId);
+    }
 }
