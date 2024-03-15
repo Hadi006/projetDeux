c79179d529940add4f67c1a3b868e6ebaee4557c
umi2002 <jonathanhoang2002@gmail.com>
2024-03-14 23:33:47 -0400

    Tests setup for join room page

--- client/src/app/pages/join-room-page/join-room-page.component.spec.ts
+++ client/src/app/pages/join-room-page/join-room-page.component.spec.ts
@@ -1,15 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PlayerService } from '@app/services/player.service';
 
 import { JoinRoomPageComponent } from './join-room-page.component';
 
 describe('JoinRoomPageComponent', () => {
     let component: JoinRoomPageComponent;
     let fixture: ComponentFixture<JoinRoomPageComponent>;
+    let playerServiceSpy: jasmine.SpyObj<PlayerService>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
     beforeEach(() => {
+        playerServiceSpy = jasmine.createSpyObj('PlayerService', ['cleanUp', 'joinGame', 'handleSockets']);
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    });
+
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [JoinRoomPageComponent],
+            imports: [FormsModule],
+            providers: [
+                { provide: PlayerService, useValue: playerServiceSpy },
+                { provide: Router, useValue: routerSpy },
+            ],
         });
+    }));
+
+    beforeEach(() => {
         fixture = TestBed.createComponent(JoinRoomPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
