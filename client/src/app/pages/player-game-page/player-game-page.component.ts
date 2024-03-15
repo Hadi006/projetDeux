657e31bf40b5babda1f6780e79dcd64e6344ab5d
umi2002 <jonathanhoang2002@gmail.com>
2024-03-15 09:45:38 -0400

    Added game countdown component and passes the correct values in player game view

--- client/src/app/pages/player-game-page/player-game-page.component.ts
+++ client/src/app/pages/player-game-page/player-game-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PlayerService } from '@app/services/player.service';
 
 @Component({
-  selector: 'app-player-game-page',
-  templateUrl: './player-game-page.component.html',
-  styleUrls: ['./player-game-page.component.scss']
+    selector: 'app-player-game-page',
+    templateUrl: './player-game-page.component.html',
+    styleUrls: ['./player-game-page.component.scss'],
 })
 export class PlayerGamePageComponent {
-
+    constructor(public playerService: PlayerService) {}
 }
