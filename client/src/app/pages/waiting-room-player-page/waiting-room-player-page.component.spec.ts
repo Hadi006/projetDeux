65aaba3ec1747a59d6d7aa93c01df9ba0255564b
umi2002 <jonathanhoang2002@gmail.com>
2024-03-15 00:13:03 -0400

    Tests setup for waiting room player page

--- client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.spec.ts
+++ client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.spec.ts
@@ -1,15 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { PlayerService } from '@app/services/player.service';
 
 import { WaitingRoomPlayerPageComponent } from './waiting-room-player-page.component';
 
 describe('WaitingRoomPlayerPageComponent', () => {
     let component: WaitingRoomPlayerPageComponent;
     let fixture: ComponentFixture<WaitingRoomPlayerPageComponent>;
+    let playerServiceSpy: jasmine.SpyObj<PlayerService>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
     beforeEach(() => {
+        playerServiceSpy = jasmine.createSpyObj('PlayerService', ['leaveGame', 'cleanUp']);
+        Object.defineProperty(playerServiceSpy, 'pin', { get: () => '1234', configurable: true });
+        Object.defineProperty(playerServiceSpy, 'gameTitle', { get: () => 'Test Game', configurable: true });
+        Object.defineProperty(playerServiceSpy, 'players', { get: () => [], configurable: true });
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    });
+
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [WaitingRoomPlayerPageComponent],
+            providers: [
+                { provide: PlayerService, useValue: playerServiceSpy },
+                { provide: Router, useValue: routerSpy },
+            ],
         });
+    }));
+
+    beforeEach(() => {
         fixture = TestBed.createComponent(WaitingRoomPlayerPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
