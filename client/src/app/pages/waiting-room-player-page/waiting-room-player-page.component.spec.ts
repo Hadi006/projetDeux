d371f40d823f73deb59ff7a797dc1c727f3e3a23
umi2002 <jonathanhoang2002@gmail.com>
2024-03-15 00:12:08 -0400

    Formatting

--- client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.spec.ts
+++ client/src/app/pages/waiting-room-player-page/waiting-room-player-page.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WaitingRoomPlayerPageComponent } from './waiting-room-player-page.component';
 
 describe('WaitingRoomPlayerPageComponent', () => {
-  let component: WaitingRoomPlayerPageComponent;
-  let fixture: ComponentFixture<WaitingRoomPlayerPageComponent>;
+    let component: WaitingRoomPlayerPageComponent;
+    let fixture: ComponentFixture<WaitingRoomPlayerPageComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [WaitingRoomPlayerPageComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [WaitingRoomPlayerPageComponent],
+        });
+        fixture = TestBed.createComponent(WaitingRoomPlayerPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(WaitingRoomPlayerPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
