a3d9437678f962867222dbd94a4f5108d8bee465
umi2002 <jonathanhoang2002@gmail.com>
2024-03-16 16:56:10 -0400

    Added test for end game subscription in player game page

--- client/src/app/pages/player-game-page/player-game-page.component.spec.ts
+++ client/src/app/pages/player-game-page/player-game-page.component.spec.ts
@@ -1,8 +1,10 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
 import { ChatboxComponent } from '@app/components/chatbox/chatbox.component';
 import { GameCountDownComponent } from '@app/components/game-count-down/game-count-down.component';
 import { QuestionComponent } from '@app/components/question/question.component';
 import { PlayerService } from '@app/services/player.service';
+import { Subject } from 'rxjs';
 
 import { PlayerGamePageComponent } from './player-game-page.component';
 
@@ -10,15 +12,27 @@ describe('PlayerGamePageComponent', () => {
     let component: PlayerGamePageComponent;
     let fixture: ComponentFixture<PlayerGamePageComponent>;
     let playerServiceSpy: jasmine.SpyObj<PlayerService>;
+    let dialogSpy: jasmine.SpyObj<MatDialog>;
 
     beforeEach(() => {
         playerServiceSpy = jasmine.createSpyObj('PlayerService', ['gameTitle', 'leaveGame']);
+        const endGameSubject = new Subject<void>();
+        Object.defineProperty(playerServiceSpy, 'endGameSubject', {
+            get: () => {
+                return endGameSubject;
+            },
+            configurable: true,
+        });
+        dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
     });
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [PlayerGamePageComponent, GameCountDownComponent, QuestionComponent, ChatboxComponent],
-            providers: [{ provide: PlayerService, useValue: playerServiceSpy }],
+            providers: [
+                { provide: PlayerService, useValue: playerServiceSpy },
+                { provide: MatDialog, useValue: dialogSpy },
+            ],
         }).compileComponents();
     }));
 
@@ -30,6 +44,13 @@ describe('PlayerGamePageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should open a dialog when the endGameSubject emits', () => {
+        playerServiceSpy.endGameSubject.subscribe(() => {
+            expect(dialogSpy.open).toHaveBeenCalled();
+        });
+        playerServiceSpy.endGameSubject.next();
     });
 
     it('stopCountDown should set isCountingDown to false', () => {
@@ -45,5 +66,13 @@ describe('PlayerGamePageComponent', () => {
             configurable: true,
         });
         expect(component.gameTitle()).toEqual('test');
+    });
+
+    it('ngOnDestroy should unsubscribe from the endGameSubscription', () => {
+        playerServiceSpy.endGameSubject.subscribe(() => {
+            expect(dialogSpy.open).not.toHaveBeenCalled();
+        });
+        component.ngOnDestroy();
+        playerServiceSpy.endGameSubject.next();
     });
 });
