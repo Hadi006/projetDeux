0a8fd311da83fe61424f51f7cd3fa2cfa31bf1e8
umi2002 <jonathanhoang2002@gmail.com>
2024-03-16 12:34:08 -0400

    Display quitters with their names striked through

--- client/src/app/pages/host-game-page/host-game-page.component.ts
+++ client/src/app/pages/host-game-page/host-game-page.component.ts
@@ -38,4 +38,8 @@ export class HostGamePageComponent {
     getPlayers() {
         return this.hostService.game.players;
     }
+
+    getQuitters() {
+        return this.hostService.quitters;
+    }
 }
