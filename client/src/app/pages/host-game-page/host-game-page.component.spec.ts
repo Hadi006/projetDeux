cd4e0991459607ad893489bab1a14e3f838c9a6d
umi2002 <jonathanhoang2002@gmail.com>
2024-03-16 14:08:14 -0400

    Setup tests for host game page

--- client/src/app/pages/host-game-page/host-game-page.component.spec.ts
+++ client/src/app/pages/host-game-page/host-game-page.component.spec.ts
@@ -1,15 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HostService } from '@app/services/host.service';
 
 import { HostGamePageComponent } from './host-game-page.component';
 
 describe('HostGamePageComponent', () => {
     let component: HostGamePageComponent;
     let fixture: ComponentFixture<HostGamePageComponent>;
+    let hostServiceSpy: jasmine.SpyObj<HostService>;
 
     beforeEach(() => {
+        hostServiceSpy = jasmine.createSpyObj('HostService', ['getCurrentQuestion', 'getTime', 'questionEnded', 'nextQuestion', 'getGame']);
+    });
+
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [HostGamePageComponent],
-        });
+            providers: [{ provide: HostService, useValue: hostServiceSpy }],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
         fixture = TestBed.createComponent(HostGamePageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
