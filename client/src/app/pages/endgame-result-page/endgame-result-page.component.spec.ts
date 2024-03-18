18cffe64d406c2f155e975ae03a483be62ad64b8
umi2002 <jonathanhoang2002@gmail.com>
2024-03-18 01:06:39 -0400

    Tests setup for endgame result page

--- client/src/app/pages/endgame-result-page/endgame-result-page.component.spec.ts
+++ client/src/app/pages/endgame-result-page/endgame-result-page.component.spec.ts
@@ -1,15 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TEST_GAME_DATA } from '@common/constant';
+import { of } from 'rxjs';
 
 import { EndgameResultPageComponent } from './endgame-result-page.component';
 
 describe('EndgameResultPageComponent', () => {
     let component: EndgameResultPageComponent;
     let fixture: ComponentFixture<EndgameResultPageComponent>;
+    let routeSpy: jasmine.SpyObj<ActivatedRoute>;
+    let routerSpy: jasmine.SpyObj<Router>;
 
     beforeEach(() => {
+        const queryParamsMap = of({ game: JSON.stringify(TEST_GAME_DATA) });
+        routeSpy = jasmine.createSpyObj('ActivatedRoute', [], {
+            queryParams: queryParamsMap,
+        });
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    });
+
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [EndgameResultPageComponent],
-        });
+            providers: [
+                { provide: ActivatedRoute, useValue: routeSpy },
+                { provide: Router, useValue: routerSpy },
+            ],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
         fixture = TestBed.createComponent(EndgameResultPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
