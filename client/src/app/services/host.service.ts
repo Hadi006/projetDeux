bf633bcf5dd5c29b60a8228bcb3d24bfed424062
umi2002 <jonathanhoang2002@gmail.com>
2024-03-18 00:56:04 -0400

    Removed comments

--- client/src/app/services/host.service.ts
+++ client/src/app/services/host.service.ts
@@ -18,8 +18,8 @@ export class HostService {
     private internalNAnswered = 0;
     private internalQuestionEnded: boolean;
     private timerId: number;
-    private internalQuestionEndedSubject = new Subject<void>(); // les subjects sont faits pour etre observes, ils devraient etre public
-    private internalGameEndedSubject = new Subject<void>(); // les subjects sont faits pour etre observes, ils devraient etre public
+    private internalQuestionEndedSubject = new Subject<void>();
+    private internalGameEndedSubject = new Subject<void>();
     private currentQuestionIndex: number;
     private internalQuitters: Player[] = [];
     private internalHistograms: HistogramData[] = [];
@@ -44,11 +44,11 @@ export class HostService {
         return this.internalQuestionEnded;
     }
     get questionEndedSubject() {
-        return this.internalQuestionEndedSubject; // utilise public readonly a la place
+        return this.internalQuestionEndedSubject;
     }
 
     get gameEndedSubject() {
-        return this.internalGameEndedSubject; // utilise public readonly a la place
+        return this.internalGameEndedSubject;
     }
 
     get quitters() {
@@ -84,7 +84,7 @@ export class HostService {
 
     leaveGame() {
         this.cleanUp();
-        this.router.navigate(['/']); // peut faire dans une composante a la place
+        this.router.navigate(['/']);
     }
 
     toggleLock() {
@@ -215,7 +215,7 @@ export class HostService {
 
     private emitEndGame() {
         this.webSocketService.emit<string>('end-game', this.internalGame.pin, (game: unknown) => {
-            this.router.navigate(['/endgame'], { queryParams: { game: JSON.stringify(game) } }); // possible de faire dans une composante (avec Observable<Game>)
+            this.router.navigate(['/endgame'], { queryParams: { game: JSON.stringify(game) } });
             this.cleanUp();
         });
     }
