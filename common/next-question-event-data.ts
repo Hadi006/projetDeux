29fb208628f313c3fa2bea1a8016a0aacfbb6744
umi2002 <jonathanhoang2002@gmail.com>
2024-03-19 10:10:49 -0400

    Fixed imports and made question optional for interface

--- common/next-question-event-data.ts
+++ common/next-question-event-data.ts
@@ -1,8 +1,8 @@
-import { Question } from '@common/question';
+import { Question } from '@common/quiz';
 import { HistogramData } from '@common/histogram-data';
 
 export interface NextQuestionEventData {
-    question: Question,
+    question?: Question,
     countdown: number,
     histogram: HistogramData
 }
