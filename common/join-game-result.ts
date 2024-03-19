45dcaee001f4b79f095228ec60bce5bf42825fe2
BaiWuLi <libaiwu59678@gmail.com>
2024-03-19 10:01:50 -0400

    refactor gameService

---
+++ common/join-game-result.ts
@@ -0,0 +1,21 @@
+import { Game } from './game';
+import { Player } from './player';
+
+interface JoinGameResult {
+    player: Player,
+    gameTitle: string,
+    otherPlayers: string[],
+    error: string,
+}
+
+class JoinGameResult implements JoinGameResult {
+    constructor(error: string, player: Player, game?: Game) {
+        this.player = player;
+        this.gameTitle = game?.quiz?.title || '';
+        this.otherPlayers = game?.players?.map((player) => player.name) || [];
+        this.error = error;
+    }
+}
+
+export { JoinGameResult };
+
