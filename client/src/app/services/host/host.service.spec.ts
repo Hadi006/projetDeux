a7a135ae4450f5ad4ced158102986cc9e2822b5a
umi2002 <jonathanhoang2002@gmail.com>
2024-03-31 11:24:55 -0400

    Added test for constructor in host service

--- client/src/app/services/host/host.service.spec.ts
+++ client/src/app/services/host/host.service.spec.ts
@@ -1,21 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { HostService } from '@app/services/host/host.service';
 import { TimeService } from '@app/services/time/time.service';
 import { HostSocketService } from '@app/services/host-socket/host-socket.service';
-import { Subject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 
 describe('HostService', () => {
     let service: HostService;
     let hostSocketServiceSpy: jasmine.SpyObj<HostSocketService>;
     let timeServiceSpy: jasmine.SpyObj<TimeService>;
     let routerSpy: jasmine.SpyObj<Router>;
+    let eventSubject: ReplaySubject<NavigationEnd>;
 
     beforeEach(async () => {
         timeServiceSpy = jasmine.createSpyObj('TimeService', ['createTimerById', 'stopTimerById', 'startTimerById', 'setTimeById', 'getTimeById']);
         timeServiceSpy.createTimerById.and.returnValue(1);
 
-        const eventSubject = new Subject<void>();
+        hostSocketServiceSpy = jasmine.createSpyObj('HostSocketService', ['disconnect']);
+
+        eventSubject = new ReplaySubject();
         routerSpy = jasmine.createSpyObj('Router', ['navigate']);
         Object.defineProperty(routerSpy, 'events', {
             get: () => {
@@ -34,9 +37,21 @@ describe('HostService', () => {
             ],
         });
         service = TestBed.inject(HostService);
+        spyOn(service, 'reset');
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it("should verify if the route uses sockets and clean up if it doesn't", () => {
+        Object.defineProperty(routerSpy, 'routerState', {
+            get: () => ({
+                snapshot: { root: { firstChild: { data: { usesSockets: false } } } },
+            }),
+        });
+        spyOn(service, 'cleanUp');
+        eventSubject.next(new NavigationEnd(1, 'url', 'url'));
+        expect(service.cleanUp).toHaveBeenCalled();
     });
 });
