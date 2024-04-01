d76e4ecd18a3ade9f38999d78de755d4754e05eb
umi2002 <jonathanhoang2002@gmail.com>
2024-03-31 21:11:42 -0400

    Formatting

--- client/src/app/services/host-socket/host-socket.service.spec.ts
+++ client/src/app/services/host-socket/host-socket.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { HostSocketService } from './host-socket.service';
 
 describe('HostSocketService', () => {
-  let service: HostSocketService;
+    let service: HostSocketService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HostSocketService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(HostSocketService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
