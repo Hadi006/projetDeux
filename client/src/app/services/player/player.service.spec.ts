48f7f936b62cd8d9cbb0ceb1c44a0ec68cc2ac62
umi2002 <jonathanhoang2002@gmail.com>
2024-03-31 22:44:00 -0400

    Added tests for verifying sockets usage in player service

--- client/src/app/services/player/player.service.spec.ts
+++ client/src/app/services/player/player.service.spec.ts
@@ -1,22 +1,23 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { PlayerService } from '@app/services/player/player.service';
 import { TimeService } from '@app/services/time/time.service';
 import { PlayerSocketService } from '@app/services/player-socket/player-socket.service';
-import { Subject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 
 describe('PlayerService', () => {
     let service: PlayerService;
     let playerSocketServiceSpy: jasmine.SpyObj<PlayerSocketService>;
     let timeServiceSpy: jasmine.SpyObj<TimeService>;
     let routerSpy: jasmine.SpyObj<Router>;
+    let eventSubject: ReplaySubject<NavigationEnd>;
 
     beforeEach(async () => {
         timeServiceSpy = jasmine.createSpyObj('TimeService', ['createTimerById', 'startTimerById', 'stopTimerById', 'setTimeById', 'getTimeById']);
         timeServiceSpy.createTimerById.and.returnValue(1);
 
-        const eventSubject = new Subject<void>();
+        eventSubject = new ReplaySubject();
         routerSpy = jasmine.createSpyObj('Router', ['navigate']);
         Object.defineProperty(routerSpy, 'events', {
             get: () => {
@@ -40,5 +41,16 @@ describe('PlayerService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it("should verify if the route uses sockets and clean up if it doesn't", () => {
+        Object.defineProperty(routerSpy, 'routerState', {
+            get: () => ({
+                snapshot: { root: { firstChild: { data: { usesSockets: false } } } },
+            }),
+        });
+        spyOn(service, 'cleanUp');
+        eventSubject.next(new NavigationEnd(1, 'url', 'url'));
+        expect(service.cleanUp).toHaveBeenCalled();
     });
 });
