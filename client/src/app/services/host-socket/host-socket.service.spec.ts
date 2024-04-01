1d37f9547ef5f4ffe271ae741fcac4d06fb04c20
umi2002 <jonathanhoang2002@gmail.com>
2024-03-31 21:14:35 -0400

    Set up for tests of host socket service

--- client/src/app/services/host-socket/host-socket.service.spec.ts
+++ client/src/app/services/host-socket/host-socket.service.spec.ts
@@ -1,9 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HostSocketService } from './host-socket.service';
+import { WebSocketService } from '@app/services/web-socket/web-socket.service';
+import { SocketTestHelper } from '@app/test/socket-test-helper';
+import { Socket } from 'socket.io-client';
 
+class WebSocketServiceMock extends WebSocketService {
+    override connect() {
+        return;
+    }
+}
 describe('HostSocketService', () => {
     let service: HostSocketService;
+    let webSocketServiceMock: WebSocketServiceMock;
+    let socketHelper: SocketTestHelper;
+
+    beforeEach(async () => {
+        socketHelper = new SocketTestHelper();
+        webSocketServiceMock = new WebSocketServiceMock();
+        webSocketServiceMock['socket'] = socketHelper as unknown as Socket;
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
