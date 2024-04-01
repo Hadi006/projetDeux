e2ae3d4c47e8c229709c6f07039d79f40b1eb8ee
umi2002 <jonathanhoang2002@gmail.com>
2024-03-31 23:34:43 -0400

    Formatting

--- client/src/app/services/player-socket/player-socket.service.spec.ts
+++ client/src/app/services/player-socket/player-socket.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { PlayerSocketService } from './player-socket.service';
 
 describe('PlayerSocketService', () => {
-  let service: PlayerSocketService;
+    let service: PlayerSocketService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PlayerSocketService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(PlayerSocketService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
