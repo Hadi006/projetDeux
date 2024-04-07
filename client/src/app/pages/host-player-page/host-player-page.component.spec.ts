99b7704c9182788d7998b283ce9c3a05f63ea215
umi2002 <jonathanhoang2002@gmail.com>
2024-04-07 19:46:03 -0400

    Added providers in tests for host player page

--- client/src/app/pages/host-player-page/host-player-page.component.spec.ts
+++ client/src/app/pages/host-player-page/host-player-page.component.spec.ts
@@ -1,16 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { QuestionComponent } from '@app/components/question/question.component';
+import { HostService } from '@app/services/host/host.service';
+import { PlayerService } from '@app/services/player/player.service';
 
 import { HostPlayerPageComponent } from './host-player-page.component';
 
 describe('HostPlayerPageComponent', () => {
     let component: HostPlayerPageComponent;
     let fixture: ComponentFixture<HostPlayerPageComponent>;
+    let playerServiceSpy: jasmine.SpyObj<PlayerService>;
+    let hostServiceSpy: jasmine.SpyObj<HostService>;
+    let routerSpy: jasmine.SpyObj<Router>;
+
+    beforeEach(() => {
+        playerServiceSpy = jasmine.createSpyObj('PlayerService', ['cleanUp']);
+        hostServiceSpy = jasmine.createSpyObj('HostService', [
+            'cleanUp',
+            'isConnected',
+            'handleSockets',
+            'requestGame',
+            'startGame',
+            'nextQuestion',
+            'endGame',
+        ]);
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [HostPlayerPageComponent, QuestionComponent],
+            providers: [
+                { provide: PlayerService, useValue: playerServiceSpy },
+                { provide: HostService, useValue: hostServiceSpy },
+                { provide: Router, useValue: routerSpy },
+            ],
         });
+    });
+
+    beforeEach(() => {
         fixture = TestBed.createComponent(HostPlayerPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
