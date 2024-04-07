f0ea3df852019eb2725bfea09d6ac4d0aeba65e6
umi2002 <jonathanhoang2002@gmail.com>
2024-04-07 19:42:53 -0400

    Formatting

--- client/src/app/pages/host-player-page/host-player-page.component.spec.ts
+++ client/src/app/pages/host-player-page/host-player-page.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HostPlayerPageComponent } from './host-player-page.component';
 
 describe('HostPlayerPageComponent', () => {
-  let component: HostPlayerPageComponent;
-  let fixture: ComponentFixture<HostPlayerPageComponent>;
+    let component: HostPlayerPageComponent;
+    let fixture: ComponentFixture<HostPlayerPageComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HostPlayerPageComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HostPlayerPageComponent],
+        });
+        fixture = TestBed.createComponent(HostPlayerPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(HostPlayerPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
